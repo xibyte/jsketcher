44b910d512e5eec2f6354263b42d35f2a84fdf19
Val Erastov <xibyte@gmail.com>
2014-10-07 01:05:21 -0700

    porting LM algorithm to JavaScript

---
+++ src/cad/Java2JS.java
@@ -0,0 +1,136 @@
+package cad;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Java2JS {
+
+  List<MatchResult> comments = new ArrayList<>();
+  int initCount = 0;
+
+  public void convert() {
+
+    Scanner scanner = new Scanner(Java2JS.class.getResourceAsStream("lm.in"));
+
+    String text = scanner.useDelimiter("\\A").next();
+
+    text = rplc(text, "(?<![A-Za-z0-9_])(private|public|protected)\\s.+\\s([A-Za-z0-9_]+);", "this.%s = null;", 2);
+
+    text = rplc(text, "\\snew double\\[(.+)\\];", " arr(%s);", 1);
+    text = rplc(text, "\\snew int\\[(.+)\\];", " arr(%s);", 1);
+    text = rplc(text, "(?<![A-Za-z0-9_])(double|int)[\\[\\]]*\\s", "var ");
+
+
+//    text = rplc(text, "for\\s+\\((int|double)\\s", "for (var ");
+    text = rplc(text, "FastMath", "Math");
+    text = rplc(text, "Arrays\\.fill", "Arrays_fill");
+    text = rplc(text, "Double\\.NEGATIVE_INFINITY", "Number.NEGATIVE_INFINITY");
+
+
+    text = rplc(text, "(protected|public|private)\\s[a-zA-Z0-9_]+\\s([a-zA-Z0-9_]+)\\(",
+        "this.%s = function(", 2);
+
+
+    text = rplc(text, "new\\sPointVectorValuePair\\((.+)\\)", "[%s]", 1);
+    text = rplc(text, "PointVectorValuePair\\s", "var ");
+
+    text = rplcParams(text);
+    text = rplc(text, "final ", "", 0);
+    text = rplc(text, "@Override", "", 0);
+
+    System.out.println(text);
+  }
+
+  private void buildComment(String text) {
+
+    comments.clear();
+
+    Pattern p = Pattern.compile("(?m)//.+$");
+    Matcher matcher = p.matcher(text);
+
+    while (matcher.find()) {
+      comments.add(matcher.toMatchResult());
+    }
+
+    p = Pattern.compile("(?s)/\\*.+?\\*/");
+    matcher = p.matcher(text);
+
+    while (matcher.find()) {
+      comments.add(matcher.toMatchResult());
+    }
+
+  }
+
+  private String rplc(String text, String pattern, String replacement, int... groups) {
+    buildComment(text);
+
+    Pattern p = Pattern.compile(pattern);
+    Matcher matcher = p.matcher(text);
+
+    StringBuffer out = new StringBuffer();
+
+    while (matcher.find()) {
+
+      if (isComment(matcher)) {
+        continue;
+      }
+
+      String[] params = new String[groups.length];
+      for (int i = 0; i < groups.length; i++) {
+        params[i] = matcher.group(groups[i]);
+      }
+      matcher.appendReplacement(out, String.format(replacement, params));
+    }
+    matcher.appendTail(out);
+    return out.toString();
+  }
+
+  private boolean isComment(MatchResult matcher) {
+    for (MatchResult comment : comments) {
+      if (matcher.start() >= comment.start() && matcher.start() < comment.end()) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+
+  private String rplcParams(String text) {
+
+    buildComment(text);
+
+    Pattern p = Pattern.compile("this\\.[^\\(]+=\\s*function\\s*\\((.+)\\)");
+    Matcher matcher = p.matcher(text);
+
+    List<MatchResult> replacements = new ArrayList<>();
+
+    while (matcher.find()) {
+      replacements.add(matcher.toMatchResult());
+    }
+
+    Collections.reverse(replacements);
+
+    int off = text.length();
+    StringBuilder out = new StringBuilder();
+    for (MatchResult m : replacements) {
+      if (isComment(m)) continue;
+      out.insert(0, text.substring(m.end(1), off));
+      String sst = m.group();
+      out.insert(0, sst.replaceAll("(?<![A-Za-z0-9_])(double|int)[\\[\\]]*\\s", ""));
+      off = m.start();
+    }
+
+    out.insert(0, text.substring(0, off));
+    return out.toString();
+  }
+
+  public static void main(String[] args) {
+    new Java2JS().convert();
+  }
+
+}
