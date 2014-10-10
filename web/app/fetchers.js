79e428d74946ca8d5b0bb3087b7bf49948fa762d
Val Erastov <xibyte@gmail.com>
2014-10-10 01:09:25 -0700

    add radius equal constraint

---
+++ web/app/fetchers.js
@@ -0,0 +1,60 @@
+TCAD.TWO.ParametricManager.prototype._fetchTwoPoints = function(objs) {
+  var points = [];
+  for (var i = 0; i < objs.length; ++i) {
+    if (objs[i]._class == 'TCAD.TWO.EndPoint') {
+      points.push(objs[i]);
+    } else if (objs[i]._class == 'TCAD.TWO.Segment') {
+      points.push(objs[i].a);
+      points.push(objs[i].b);
+    }
+  }
+  if (points.length < 2) {
+    throw "Illegal Argument. Constraint requires 2 points or 1 line."
+  }
+  return points;
+};
+
+TCAD.TWO.ParametricManager.prototype._fetchTwoOrMoreArcs = function(objs) {
+  var arcs = [];
+  for (var i = 0; i < objs.length; ++i) {
+    if (objs[i]._class == 'TCAD.TWO.Arc') {
+      arcs.push(objs[i]);
+    }
+  }
+  if (arcs.length < 2) {
+    throw "Illegal Argument. Constraint requires ata least 2 arcs."
+  }
+  return arcs;
+};
+
+TCAD.TWO.ParametricManager.prototype._fetchPointAndLine = function(objs) {
+
+  var point = null;
+  var line = null;
+
+  for (var i = 0; i < objs.length; ++i) {
+    if (objs[i]._class == 'TCAD.TWO.EndPoint') {
+      point = objs[i];
+    } else if (objs[i]._class == 'TCAD.TWO.Segment') {
+      line = objs[i];
+    }
+  }
+  if (point == null || line == null) {
+    throw "Illegal Argument. Constraint requires point and line."
+  }
+
+  return [point, line];
+};
+
+TCAD.TWO.ParametricManager.prototype._fetchTwoLines = function(objs) {
+  var lines = [];
+  for (var i = 0; i < objs.length; ++i) {
+    if (objs[i]._class == 'TCAD.TWO.Segment') {
+      lines.push(objs[i]);
+    }
+  }
+  if (lines.length < 2) {
+    throw "Illegal Argument. Constraint requires 2 lines."
+  }
+  return lines;
+};
