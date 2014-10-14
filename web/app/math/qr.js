99d6ca11d403a469a4475b0c1e10321408741478
Val Erastov <xibyte@gmail.com>
2014-10-14 00:59:40 -0700

    add qr decomposition

---
+++ web/app/math/qr.js
@@ -0,0 +1,85 @@
+this.qrDecomposition = function(matrix) {
+
+  function arr(size) {
+    var out = [];
+    out.length = size;
+    for (var i = 0; i < size; ++i) {
+      out[i] = 0;
+    }
+    return out;
+  }
+
+  this.qrRankingThreshold = 1e-30; //??
+  this.solvedCols  = Math.min(nR, nC);
+  this.diagR     = arr(nC);
+  this.norm    = arr(nC);
+  this.beta    = arr(nC);
+  this.permutation = arr(nC);
+  this.rank = null;
+
+  var nR = this.matrix.length;
+  var nC = this.matrix[0].length;
+
+  // initializations
+  for (var k = 0; k < nC; ++k) {
+    this.permutation[k] = k;
+    var norm2 = 0;
+    for (var i = 0; i < nR; ++i) {
+      var akk = matrix[i][k];
+      norm2 += akk * akk;
+    }
+    this.norm[k] = Math.sqrt(norm2);
+  }
+
+  // transform the matrix column after column
+  for (var k = 0; k < nC; ++k) {
+
+    // select the column with the greatest norm on active components
+    var nextColumn = -1;
+    var ak2 = Number.NEGATIVE_INFINITY;
+    for (var i = k; i < nC; ++i) {
+      var norm2 = 0;
+      for (var j = k; j < nR; ++j) {
+        var aki = matrix[j][this.permutation[i]];
+        norm2 += aki * aki;
+      }
+      if (!isFinite(norm2)) {
+        throw "UNABLE_TO_PERFORM_QR_DECOMPOSITION";
+      }
+      if (norm2 > ak2) {
+        nextColumn = i;
+        ak2    = norm2;
+      }
+    }
+    if (ak2 <= this.qrRankingThreshold) {
+      this.rank = k;
+      return;
+    }
+    var pk          = this.permutation[nextColumn];
+    this.permutation[nextColumn] = this.permutation[k];
+    this.permutation[k]      = pk;
+
+    // choose alpha such that Hk.u = alpha ek
+    var akk   = matrix[k][pk];
+    var alpha = (akk > 0) ? -Math.sqrt(ak2) : Math.sqrt(ak2);
+    var betak = 1.0 / (ak2 - akk * alpha);
+    this.beta[pk]   = betak;
+
+    // transform the current column
+    this.diagR[pk]    = alpha;
+    matrix[k][pk] -= alpha;
+
+    // transform the remaining columns
+    for (var dk = nC - 1 - k; dk > 0; --dk) {
+      var gamma = 0;
+      for (var j = k; j < nR; ++j) {
+        gamma += matrix[j][pk] * matrix[j][this.permutation[k + dk]];
+      }
+      gamma *= betak;
+      for (var j = k; j < nR; ++j) {
+        matrix[j][this.permutation[k + dk]] -= gamma * matrix[j][pk];
+      }
+    }
+  }
+  this.rank = this.solvedCols;
+}
