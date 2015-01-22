2cc244d340315caa03fa11e3ab1e56553abb0a31
Val Erastov <xibyte@gmail.com>
2015-01-22 00:07:02 -0800

    some state

--- web/app/math/math.js
+++ web/app/math/math.js
@@ -27,3 +27,58 @@ TCAD.math.distance = function(x1, y1, x2, y2) {
   var dy = y1 - y2;
   return Math.sqrt(dx * dx + dy * dy);
 };
+
+TCAD.math.ORIGIN = new TCAD.Vector(0, 0, 0);
+
+TCAD.math.AXIS = {
+  X : new TCAD.Vector(1, 0, 0),
+  Y : new TCAD.Vector(0, 1, 0),
+  Z : new TCAD.Vector(0, 0, 1)
+}; 
+
+TCAD.math.rotateMatrix = function(angle, axis, pivot) {
+  var sin = Math.sin(angle);
+  var cos = Math.cos(angle);
+  var axisX, axisY, axisZ;
+  var m = new TCAD.Matrix();
+
+  var AXIS = TCAD.math.AXIS;
+  
+  if (axis === AXIS.X || axis === AXIS.Y || axis === AXIS.Z) {
+    axisX = axis.x;
+    axisY = axis.y;
+    axisZ = axis.z;
+  } else {
+    // normalize
+    var mag = axis.length();
+
+    if (mag == 0.0) {
+      return m;
+    } else {
+      axisX = axis.x / mag;
+      axisY = axis.y / mag;
+      axisZ = axis.z / mag;
+    }
+  }
+
+  var px = pivot.x;
+  var py = pivot.y;
+  var pz = pivot.z;
+
+  m.mxx = cos + axisX * axisX * (1 - cos);
+  m.mxy = axisX * axisY * (1 - cos) - axisZ * sin;
+  m.mxz = axisX * axisZ * (1 - cos) + axisY * sin;
+
+  m.tx = px * (1 - m.mxx) - py * m.mxy - pz * m.mxz;
+
+  m.myx = axisY * axisX * (1 - cos) + axisZ * sin;
+  m.myy = cos + axisY * axisY * (1 - cos);
+  m.myz = axisY * axisZ * (1 - cos) - axisX * sin;
+  m.ty = py * (1 - m.myy) - px * m.myx - pz * m.myz;
+
+  m.mzx = axisZ * axisX * (1 - cos) - axisY * sin;
+  m.mzy = axisZ * axisY * (1 - cos) + axisX * sin;
+  m.mzz = cos + axisZ * axisZ * (1 - cos);
+  m.tz = pz * (1 - m.mzz) - px * m.mzx - py * m.mzy;
+  return m;
+};
