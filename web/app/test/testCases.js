0cff9646b9a1655c3a4eb5141a9f3510fe983be3
Val <xibyte@gmail.com>
2015-02-24 01:17:53 -0800

    testing stub

--- web/app/test/testCases.js
+++ web/app/test/testCases.js
@@ -2,4 +2,9 @@
 TCAD.test.cases.roundRect = function() {
   _loadFixture("RoundRect");
   _assertEq(31, THE_APP.viewer.parametricManager.system.length);
+//  var solver = THE_APP.viewer.parametricManager.prepare([]);
+//  var status = solver.solve(1);
+//  _assertEq(1, status.returnCode);
+//  _assertEq(1, status.evalCount);
+//  _assertEq(1, status.error);
 };
