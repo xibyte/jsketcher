f79527efdc44f7db9e0df2399bf8478df5542783
Val Erastov <xibyte@gmail.com>
2015-02-24 21:20:19 -0800

    Test case for round rect

--- web/app/test/suite.js
+++ web/app/test/suite.js
@@ -11,15 +11,30 @@ TCAD.test.runSuite = function() {
 
 function _log(text) {
   $('#testOutput').append("<div>"+text+"</div>");
-};
+}
 
 _loadFixture = function(name) {
-  THE_APP.loadSketch(TCAD.test.fixtures[name]);
+  APP.loadSketch(TCAD.test.fixtures[name]);
+};
+
+_loadFixturesToLocalStorage = function() {
+  for (var p in TCAD.test.fixtures) {
+    var key = "test:" + p;
+    console.log("Storing: " + key);
+    localStorage.setItem("TCAD.projects." + key, JSON.stringify(TCAD.test.fixtures[p]));
+  }
 };
 
 _assertEq = function(expected, actual, msg) {
   if (expected !== actual) {
     _log("<b class='err'>*</b> Assertion Error. Expected [" + expected + "], but was [" + actual + "]");
+    if (!!msg) _log(msg);
+  }
+};
+
+_assertEqD = function(expected, actual, precision, msg) {
+  if (Math.abs(expected - actual) > precision) {
+    _log("<b class='err'>*</b> Assertion Error. Expected [" + expected + "] with precision " + precision + " but was [" + actual + "]");
     if (!!msg) _log(msg);
   }
 };
--- web/app/test/testCases.js
+++ web/app/test/testCases.js
@@ -1,10 +1,15 @@
 
 TCAD.test.cases.roundRect = function() {
   _loadFixture("RoundRect");
-  _assertEq(31, THE_APP.viewer.parametricManager.system.length);
-//  var solver = THE_APP.viewer.parametricManager.prepare([]);
-//  var status = solver.solve(1);
-//  _assertEq(1, status.returnCode);
-//  _assertEq(1, status.evalCount);
-//  _assertEq(1, status.error);
+  _assertEq(31, APP.viewer.parametricManager.system.length);
+  
+  var ep = APP.viewer.findById(28);
+  var pm = APP.viewer.parametricManager;
+  _assertEqD(0.00002047865, pm.prepare([]).system.error(), 0.00000000001);
+  ep.x += 30;
+  var solver = pm.prepare([]);
+  _assertEqD(60, solver.system.error(), 0.01);
+  solver.solve();
+  _assertEqD(0.000005, solver.system.error(), 1e-6);
 };
+
