b977c18a9db60ea5288621bc9dabd667990a8061
Val Erastov <xibyte@gmail.com>
2015-07-21 17:19:14 -0700

    extract API for solver

--- web/app/sketcher/parametric.js
+++ web/app/sketcher/parametric.js
@@ -60,7 +60,7 @@ TCAD.TWO.ParametricManager.prototype._add = function(constr) {
 
 TCAD.TWO.ParametricManager.prototype.checkRedundancy = function (subSystem, constr) {
   var solver = this.prepareForSubSystem([], subSystem);
-  if (TCAD.parametric.diagnose(solver.system).conflict) {
+  if (solver.diagnose().conflict) {
     alert("Most likely this "+constr.NAME + " constraint is CONFLICTING!")
   }
 };
--- web/app/test/testCases.js
+++ web/app/test/testCases.js
@@ -6,16 +6,16 @@ TCAD.test.cases.roundRect = function() {
   
   var ep = APP.viewer.findById(28);
   var pm = APP.viewer.parametricManager;
-  _assertEqD(0.00002047865, pm.prepare([]).solvers[0].system.error(), 1e-12);
+  _assertEqD(0.00002047865, pm.prepare([]).solvers[0].error(), 1e-12);
   ep.x += 30;
   var solver = pm.prepare([]).solvers[0];
-  _assertEqD(60, solver.system.error(), 0.01);
+  _assertEqD(60, solver.error(), 0.01);
   var status = solver.solve(true);
-  _assertEqD(0.000005, solver.system.error(), 1e-8);
+  _assertEqD(0.000005, solver.error(), 1e-8);
   _assertEq(12, status.evalCount);
   _assertEq(1, status.returnCode);
   var status = solver.solve(false);
-  _assertEqD(1.4575007867279055e-10, solver.system.error(), 1e-12);
+  _assertEqD(1.4575007867279055e-10, solver.error(), 1e-12);
   _assertEq(20, status.evalCount);
   _assertEq(1, status.returnCode);
 };
