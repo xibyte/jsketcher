2395ba0b91f1b7e682b5afd3e7abe202ca833582
verastov <verastov@guidewire.com>
2015-09-25 21:43:31 -0700

    live wizard for extrude/cut operations

--- web/app/3d/wizards/wizards.js
+++ web/app/3d/wizards/wizards.js
@@ -1,21 +1,34 @@
 TCAD.wizards = {};
 
+TCAD.wizards.IMAGINE_MATERIAL = new THREE.LineBasicMaterial({
+  color: 0xFA8072,
+  linewidth: 1/TCAD.DPR,
+  depthWrite: false,
+  depthTest: false
+});
+
+TCAD.wizards.BASE_MATERIAL = new THREE.LineBasicMaterial({
+  color: 0x8B0000,
+  linewidth: 3/TCAD.DPR,
+  depthWrite: false,
+  depthTest: false
+});
+
 TCAD.wizards.OpWizard = function(viewer) {
   this.previewGroup = new THREE.Object3D();
-  this.previewGroup.renderDepth = 1e20;
-  viewer.scene.add(this.previewGroup);
   this.lines = [];
+  this.viewer = viewer;
+  viewer.scene.add(this.previewGroup);
 };
 
-TCAD.wizards.OpWizard.prototype.setupLine = function(lineId, a, b) {
+TCAD.wizards.OpWizard.prototype.setupLine = function(lineId, a, b, material) {
   var line = this.lines[lineId];
   if (line === undefined) {
     var lg = new THREE.Geometry();
     lg.vertices.push(new THREE.Vector3().copy(a));
     lg.vertices.push(new THREE.Vector3().copy(b));
-    TCAD.SketchFace.prototype.WIREFRAME_MATERIAL.depthWrite = false;
-    line = new THREE.Line(lg, TCAD.SketchFace.prototype.WIREFRAME_MATERIAL);
-    line.renderDepth = 0;
+    line = new THREE.Line(lg, material);
+    line.renderOrder = 1e10;
     this.previewGroup.add(line);
     this.lines[lineId] = line;
   } else {
@@ -23,6 +36,11 @@ TCAD.wizards.OpWizard.prototype.setupLine = function(lineId, a, b) {
     line.geometry.vertices[1] = new THREE.Vector3().copy(b);
     line.geometry.verticesNeedUpdate = true;
   }
+};
+
+TCAD.wizards.OpWizard.prototype.dispose = function() {
+  this.viewer.scene.remove(this.previewGroup);
+  this.viewer.render();
 };
 
 TCAD.wizards.ExtrudeWizard = function(viewer, polygons) {
@@ -39,11 +57,11 @@ TCAD.wizards.ExtrudeWizard.prototype.update = function(target) {
     var lid = TCAD.geom.calculateExtrudedLid(poly, target, 1);
     var p, q, n = poly.length;
     for (p = n - 1, q = 0; q < n; p = q++) {
-      this.setupLine(linesCounter ++, poly[p], poly[q]);
-      this.setupLine(linesCounter ++, lid[p], lid[q]);
+      this.setupLine(linesCounter ++, poly[p], poly[q], TCAD.wizards.BASE_MATERIAL);
+      this.setupLine(linesCounter ++, lid[p], lid[q], TCAD.wizards.IMAGINE_MATERIAL);
     }
     for (q = 0; q < n; q++) {
-      this.setupLine(linesCounter ++, poly[q], lid[q]);
+      this.setupLine(linesCounter ++, poly[q], lid[q], TCAD.wizards.IMAGINE_MATERIAL);
     }
   }
 };
