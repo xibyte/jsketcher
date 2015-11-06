69583959e23e42fc4fc2061f2ab7b4a885d97b01
Val Erastov <xibyte@gmail.com>
2015-11-05 18:26:34 -0800

    add plane concept

--- web/app/3d/viewer.js
+++ web/app/3d/viewer.js
@@ -2,9 +2,9 @@ TCAD.DPR = (window.devicePixelRatio) ? window.devicePixelRatio : 1;
 
 TCAD.view = {};
 
-TCAD.view.setFaceColor = function(polyFace, color) {
-  for (var i = 0; i < polyFace.faces.length; ++i) {
-    var face = polyFace.faces[i];
+TCAD.view.setFacesColor = function(faces, color) {
+  for (var i = 0; i < faces.length; ++i) {
+    var face = faces[i];
     if (color == null) {
       face.color.set(new THREE.Color());
     } else {
@@ -48,10 +48,6 @@ TCAD.Viewer = function(bus) {
     render();
   }
   window.addEventListener( 'resize', onWindowResize, false );
-
-  /**
-   * CONTROLS
-   **/
 
 //  controls = new THREE.OrbitControls( camera , renderer.domElement);
   var trackballControls = new THREE.TrackballControls( camera , renderer.domElement);
@@ -106,19 +102,10 @@ TCAD.Viewer = function(bus) {
     updateTransformControls();
   }
 
-  /**
-   * TOOLS
-   **/
-
-  this.toolMgr = new TCAD.ToolManager(this);
-
-
-  /**
-   * FACE SELECTING
-   **/
-
-
-  this.selectionMgr = new TCAD.FaceSelectionManager( 0xFAFAD2, 0xFF0000, null);
+  this.workGroup = new THREE.Object3D();
+  this.scene.add(this.workGroup);
+  this.selectionMgr = new TCAD.SelectionManager( this, 0xFAFAD2, 0xFF0000, null);
+  var viewer = this;
 
   var raycaster = new THREE.Raycaster();
 
@@ -129,32 +116,11 @@ TCAD.Viewer = function(bus) {
 
     var mouse = new THREE.Vector3( x, y, 1 );
     raycaster.setFromCamera( mouse, camera );
-    return raycaster.intersectObjects( scene.children );
+    return raycaster.intersectObjects( viewer.workGroup.children, true );
   };
   
-  var scope = this; 
   function onClick(e) {
-    var intersects = scope.raycast(e);
-    if (intersects.length === 0) scope.transformControls.detach();
-    for (var ii = 0; ii < intersects.length; ii++) {
-      var pickResult = intersects[ii];
-      if (!pickResult.face) continue;
-      if (pickResult.face.__TCAD_polyFace !== undefined) {
-        var poly = pickResult.face.__TCAD_polyFace;
-        if (scope.selectionMgr.contains(poly)) {
-          scope.toolMgr.handleClick(poly, pickResult);
-        } else {
-          if (e.shiftKey) {
-            scope.transformControls.attach(pickResult.object);
-          } else {
-            scope.select(poly);
-            pickResult.object.geometry.colorsNeedUpdate = true;
-          }
-        }
-      }
-      render();
-      break;
-    }
+    viewer.selectionMgr.handlePick(e);
   }
   
   var mouseState = {
@@ -162,20 +128,14 @@ TCAD.Viewer = function(bus) {
     startY : 0
   };
 
-  function onMove(e) {
-  };
-  
-  renderer.domElement.addEventListener('mousemove', function(e){scope.toolMgr.handleMove(e)}, false);
-  renderer.domElement.addEventListener('mousedown', 
+  renderer.domElement.addEventListener('mousedown',
     function(e) {
       mouseState.startX = e.clientX;
       mouseState.startY = e.clientY;
-      renderer.domElement.addEventListener('mousemove', onMove, false);
     }, false);
-  
-  renderer.domElement.addEventListener('mouseup', 
+
+  renderer.domElement.addEventListener('mouseup',
     function(e) {
-      renderer.domElement.removeEventListener('mousemove', onMove);
       var dx = Math.abs(mouseState.startX - e.clientX);
       var dy = Math.abs(mouseState.startY - e.clientY);
       var TOL = 1;
@@ -194,139 +154,55 @@ TCAD.Viewer = function(bus) {
   animate();
 };
 
-TCAD.Viewer.prototype.select = function(polyFace) {
-  this.selectionMgr.select(polyFace);
-  this.bus.notify('selection', polyFace);
-};
-
-TCAD.FaceSelectionManager = function(selectionColor, readOnlyColor, defaultColor) {
+TCAD.SelectionManager = function(viewer, selectionColor, readOnlyColor, defaultColor) {
+  this.viewer = viewer;
   this.selectionColor = selectionColor;
   this.readOnlyColor = readOnlyColor;
   this.defaultColor = defaultColor;
   this.selection = [];
+  this.planeSelection = [];
 };
 
-TCAD.FaceSelectionManager.prototype.select = function(polyFace) {
+TCAD.SelectionManager.prototype.handlePick = function(event) {
+
+  var pickResults = this.viewer.raycast(event);
+  for (var i = 0; i < pickResults.length; i++) {
+    var pickResult = pickResults[i];
+    if (!!pickResult.face && pickResult.face.__TCAD_polyFace !== undefined) {
+      var sketchFace = pickResult.face.__TCAD_polyFace;
+      if (!this.contains(sketchFace)) {
+        this.select(sketchFace);
+        pickResult.object.geometry.colorsNeedUpdate = true;
+        this.viewer.bus.notify('selection', sketchFace);
+        this.viewer.render();
+        break;
+      }
+    }
+  }
+};
+
+TCAD.SelectionManager.prototype.select = function(sketchFace) {
   this.clear();
-  if (polyFace.curvedSurfaces !== null) {
-    for (var i = 0; i < polyFace.curvedSurfaces.length; i++) {
-      var face  = polyFace.curvedSurfaces[i];
+  if (sketchFace.curvedSurfaces !== null) {
+    for (var i = 0; i < sketchFace.curvedSurfaces.length; i++) {
+      var face  = sketchFace.curvedSurfaces[i];
       this.selection.push(face);
-      TCAD.view.setFaceColor(face, this.readOnlyColor);
+      TCAD.view.setFacesColor(face.faces, this.readOnlyColor);
     }
   } else {
-    this.selection.push(polyFace);
-    TCAD.view.setFaceColor(polyFace, this.selectionColor);
+    this.selection.push(sketchFace);
+    TCAD.view.setFacesColor(sketchFace.faces, this.selectionColor);
   }
-
 };
 
-TCAD.FaceSelectionManager.prototype.contains = function(polyFace) {
-  return this.selection.indexOf(polyFace) != -1;
+TCAD.SelectionManager.prototype.contains = function(face) {
+  return this.selection.indexOf(face) != -1;
 };
 
-TCAD.FaceSelectionManager.prototype.clear = function() {
+TCAD.SelectionManager.prototype.clear = function() {
   for (var i = 0; i < this.selection.length; ++ i) {
-    TCAD.view.setFaceColor(this.selection[i], this.defaultColor);
+    TCAD.view.setFacesColor(this.selection[i].faces, this.defaultColor);
   }
   this.selection.length = 0;
 };
 
-TCAD.ToolManager = function(viewer) {
-  this.viewer = viewer;
-  this.tool = null;
-};
-
-TCAD.ToolManager.prototype.handleClick = function(face, pickResult) {
-  if (this.tool == null) {
-    return;    
-  }
-  if (this.tool.workArea != face) {
-    return;    
-  }
-  if (this.tool.workArea.sketch == null) {
-    this.tool.workArea.sketch = new TCAD.Sketch();
-    pickResult.object.parent.add(this.tool.workArea.sketch.group);
-  }
-  this.tool.handleClick(face, pickResult);
-};
-
-
-TCAD.ToolManager.prototype.handleMove = function(event) {
-  if (this.tool == null) {
-    return;
-  }
-  this.tool.handleMove(event, this.viewer);
-};
-
-TCAD.ToolManager.prototype.commit = function() {
-  if (this.tool == null) {
-    return;
-  }
-  this.tool.commit();
-  this.viewer.render();
-  this.tool = null;
-};
-
-TCAD.PolygonTool = function(workArea, viewer) {
-  this.workArea = workArea;
-  this.viewer = viewer;
-  this.poly = {shell : [], holes : []};
-};
-
-TCAD.PolygonTool.prototype.handleClick = function(face, pickResult) {
-  this.poly.shell.push(new TCAD.Vector().setV(pickResult.point));
-  var point = TCAD.utils.createPoint(pickResult.point.x, pickResult.point.y, pickResult.point.z);
-  this.workArea.sketch.group.add(point);
-};
-
-TCAD.PolygonTool.prototype.handleMove = function(event, raycast) {
-
-};
-
-TCAD.PolygonTool.prototype.commit = function() {
-  var n = this.workArea.polygon.normal;
-  var _2d = new TCAD.Polygon(this.poly.shell, this.poly.holes, n);
-  
-  var solid = TCAD.utils.createSolidMesh(TCAD.geom.extrude(_2d, n.multiply(1.1)));
-  this.workArea.sketch.group.parent.add(solid);
-};
-
-
-TCAD.LineTool = function(workArea) {
-  this.workArea = workArea;
-  this.protoLine = null;
-};
-
-TCAD.LineTool.prototype.handleClick = function(face, pickResult) {
-  if (this.protoLine == null) {
-    this.protoLine = TCAD.utils.createLine(pickResult.point, pickResult.point, 0x0000ff);
-    this.workArea.sketch.group.add(this.protoLine);
-  } else {
-    
-  }
-};
-
-TCAD.LineTool.prototype.handleMove = function(event, viewer) {
-  if (this.protoLine != null) {
-    var intersects = viewer.raycast(event);
-    if (intersects.length > 0 && intersects[0].face.__TCAD_polyFace == this.workArea) {
-      var p = intersects[0].point;
-      var vertices = this.protoLine.geometry.vertices;
-      vertices[vertices.length - 1].x = p.x;
-      vertices[vertices.length - 1].y = p.y;
-      vertices[vertices.length - 1].z = p.z;
-      this.protoLine.geometry.verticesNeedUpdate = true;
-      viewer.render();
-    }
-  }
-};
-
-
-TCAD.LineTool.prototype.commit = function() {
-//  var n = this.workArea.polygon.normal;
-//  var _2d = new TCAD.Polygon(this.poly.shell, this.poly.holes, n);
-//
-//  var solid = TCAD.utils.createSolidMesh(TCAD.geom.extrude(_2d, n.multiply(1.1)));
-//  this.workArea.sketch.group.parent.add(solid);
-};
