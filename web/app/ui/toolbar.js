b42b9cafeacf5fb15d8ec3781aba0a834bd2292d
Val Erastov <xibyte@gmail.com>
2016-10-09 17:38:32 -0700

    icons for save/upload etc...

--- web/app/ui/toolbar.js
+++ web/app/ui/toolbar.js
@@ -14,21 +14,45 @@ export default function ToolBar() {
 
 ToolBar.prototype.add = function(caption, icon, action) {
   var btn = $('<div>', {
-    'class': 'tc-toolbar-btn',
+    'class': 'tc-toolbar-btn tc-squeezed-text',
     text : caption,
-    css: {
+    css: ToolBar.buttonCss({
       'background-image': 'url('+icon+')',
       'background-repeat': 'no-repeat',
       'background-position-x': 'center',
       'background-position-y': 'top',
-      'background-size': '48px 48px',
-      'border-radius' : '5px',
-      'width': '53px',
-      'padding-top' : '48px',
-      'margin-top' : '5px'
-    }
+      'background-size': '48px 48px'
+    })
   });
   btn.click(action);
   this.node.append(btn);
   return btn;
+};
+
+ToolBar.prototype.addFa = function(faIcon, action) {
+  var btn = $('<div>', {
+    'class': 'tc-toolbar-btn',
+    css : {
+      'border-radius' : '5px',
+      'padding' : '5px'
+    }
+  });
+  btn.click(action);
+  btn.append($('<i>', {
+    'class' : 'fa fa-' + faIcon,
+    css: {
+      'vertical-align': 'middle'
+    }
+  }));
+  this.node.append(btn);
+  return btn;
+};
+
+ToolBar.buttonCss = function(css) {
+  return Object.assign(css, {
+    'border-radius' : '5px',
+    'width': '53px',
+    'padding-top' : '48px',
+    'margin-top' : '5px'
+  })
 };
