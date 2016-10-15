7b1a31deec2965e919257a87dd353021efee0155
Val Erastov <xibyte@gmail.com>
2016-10-14 22:27:56 -0700

    read operation info from operation definition

--- web/app/3d/operations.js
+++ web/app/3d/operations.js
@@ -1,54 +1,55 @@
+import * as math from '../math/math'
 
 export const CUT = {
   icon: 'img/3d/cut',
   label: 'Cut', 
-  info: (p) => 'CUT (' + p + ')'
+  info: (p) => '(' + math.norm2(p.target) + ')'
 };
 
 export const PAD = {
   icon: 'img/3d/extrude',
   label: 'Extrude',
-  info: (p) => 'PAD (' + p + ')'
+  info: (p) => '(' + math.norm2(p.target) + ')'
 };
 
 export const SHELL = {
   icon: 'img/3d/shell',
   label: 'Shell',
-  info: (p) => 'SHELL (' + p + ')'
+  info: (p) => '(' + p.d + ')'
 };
 
 export const BOX = {
   icon: 'img/3d/cube',
   label: 'Box',
-  info: (p) => 'BOX (' + p + ')'
+  info: (p) => '(' + p.w + ', ' + p.h + ', ' + p.d + ')'
 };
 
 export const PLANE = {
   icon: 'img/3d/plane',
   label: 'Plane',
-  info: (p) => 'PLANE (' + p + ')'
+  info: (p) =>  '(' + p.depth + ')'
 };
 
 export const SPHERE = {
   icon: 'img/3d/sphere',
   label: 'Sphere',
-  info: (p) => 'SPHERE (' + p + ')'
+  info: (p) => '(' + p.radius + ')'
 };
 
 export const INTERSECTION = {
   icon: 'img/3d/intersection',
   label: 'Intersection',
-  info: (p) => 'INTERSECTION (' + p + ')'
+  info: (p) => null
 };
 
 export const DIFFERENCE = {
   icon: 'img/3d/difference',
   label: 'Difference',
-  info: (p) => 'DIFFERENCE (' + p + ')'
+  info: (p) => null
 };
 
 export const UNION = {
   icon: 'img/3d/union',
   label: 'Union',
-  info: (p) => 'UNION (' + p + ')'
+  info: (p) => null
 };
