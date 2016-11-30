957aabe74d985ee79cba4dd756b3334f4f690369
Val Erastov <xibyte@gmail.com>
2016-11-30 02:12:54 -0800

    ellipse drawing implementation

---
+++ web/app/sketcher/shapes/elliptical-arc.js
@@ -0,0 +1,19 @@
+import {Ref} from './ref'
+import {SketchObject} from './sketch-object'
+
+import * as math from '../../math/math';
+
+export class EllipticalArc extends SketchObject {
+
+  constructor(ep1, ep2, a, b) {
+  }
+
+  drawImpl(ctx, scale) {
+  }
+
+
+  normalDistance(aim) {
+  }
+  
+}
+EllipticalArc.prototype._class = 'TCAD.TWO.EllipticalArc';
