a7d7ef5cc3f722f37d696e6080d8fc37dd126b42
Val Erastov <xibyte@gmail.com>
2016-12-09 11:40:16 -0800

    bezier curve math refactoring

--- web/app/math/bezier-cubic.js
+++ web/app/math/bezier-cubic.js
@@ -2,45 +2,33 @@ import Vector from './vector'
 import * as math from './math'
 
 export function LUT(a, b, cp1, cp2, scale) {
-  scale = 1/scale;
-
-  const rotation = Math.atan2(b.y - a.y, b.x - b.x);
-
-  const tr = (p) => rotate(p, rotation);
-  const trBack = (p) => rotate(p, -rotation);
-  
-  const curve = new CurveData(tr(a), tr(b), tr(cp1), tr(cp2));
-  
+  scale = 1 / scale;
   const lut = [a];
   for (let t = 0; t < 1; t += 0.1 * scale) {
-    const p = compute(t, curve);
-    lut.push(trBack(p));
+    const p = compute(t, a, b, cp1, cp2);
+    lut.push(p);
   }
   lut.push(b);
   return lut;
 }
 
-function rotate(point, angle) {
-  return math.rotate(point.x, point.y, angle);
-} 
+export function compute(t, from, to, controlPoint1, controlPoint2) {
+  const mt = 1 - t;
+  const mt2 = mt * mt;
+  const t2 = t * t;
 
-export function compute(t, curve) {
-  const mt = 1-t;
-  const mt2 = mt*mt;
-  const t2 = t*t;
-
-  const a = mt2*mt;
-  const b = mt2*t*3;
-  const c = mt*t2*3;
-  const d = t*t2;
-  const p0 = curve.a;
-  const p3 = curve.b;
-  const p1 = curve.cp1;
-  const p2 = curve.cp2;
+  const a = mt2 * mt;
+  const b = mt2 * t * 3;
+  const c = mt * t2 * 3;
+  const d = t * t2;
+  const p0 = from;
+  const p3 = to;
+  const p1 = controlPoint1;
+  const p2 = controlPoint2;
   return new Vector(
-    a*p0.x + b*p1.x + c*p2.x + d*p3.x,
-    a*p0.y + b*p1.y + c*p2.y + d*p3.y,
-    a*p0.z + b*p1.z + c*p2.z + d*p3.z
+    a * p0.x + b * p1.x + c * p2.x + d * p3.x,
+    a * p0.y + b * p1.y + c * p2.y + d * p3.y,
+    a * p0.z + b * p1.z + c * p2.z + d * p3.z
   );
 }
 
