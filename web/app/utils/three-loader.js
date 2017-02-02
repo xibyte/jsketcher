44d99e803498aa13b9f57caa9babe3c07997869b
Val Erastov <xibyte@gmail.com>
2017-02-01 16:47:27 -0800

    load THREE.js using webpack

---
+++ web/app/utils/three-loader.js
@@ -0,0 +1,6 @@
+import THREE from '../../lib/three/three'
+window.THREE = THREE;
+require('../../lib/three/TrackballControls');
+require('../../lib/three/OrbitControls');
+require('../../lib/three/TransformControls');
+
