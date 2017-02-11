2a5754fb96d5cedbf5771adad87d7851d21d1cd8
Val Erastov <xibyte@gmail.com>
2017-02-10 21:50:28 -0800

    brep debug framework / fix volumes / enable add shell on scene back

--- web/app/3d/modeler-app.js
+++ web/app/3d/modeler-app.js
@@ -76,7 +76,7 @@ function App() {
 
 App.prototype.addShellOnScene = function(shell, skin) {
   const sceneSolid = new BREPSceneSolid(shell, undefined, skin);
-  //this.viewer.workGroup.add(sceneSolid.cadGroup);
+  this.viewer.workGroup.add(sceneSolid.cadGroup);
   this.viewer.render();
   return sceneSolid;
 };
