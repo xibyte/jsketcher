7bf2c7b53a7d47b615e17fa437134440ccbc36d7
Val Erastov <xibyte@gmail.com>
2017-12-18 22:06:43 -0800

    boolean debug

--- web/app/brep/debug/debugger/section.jsx
+++ web/app/brep/debug/debugger/section.jsx
@@ -12,12 +12,14 @@ export default class Section extends React.PureComponent {
   }
 
   render() {
-    let {name, tabs, closable, defaultClosed, accent, children, captionStyles} = this.props;
+    let {name, tabs, closable, defaultClosed, accent, children, captionStyles, controls} = this.props;
     let closed = closable && (this.state.closed || defaultClosed);
     return <div className={cx('section', {closable, closed})} style={{paddingLeft: tabs}}>
       <div className={cx('caption', {accent}, captionStyles)} >
-        {closable && <i className={'fa fa-fw fa-caret-' + (closed ? 'right': 'down')} />} {name}</div>
-        {children}
+        <span className='title'>{closable && <i className={'fa fa-caret-' + (closed ? 'right': 'down')} />} {name}</span>
+        <span className='control'>{controls}</span>
+      </div>
+      {children}
     </div>;
   }
 }
