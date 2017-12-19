75221e0729388d9425bacaceeb9647ced0ec9bfb
Val Erastov <xibyte@gmail.com>
2017-12-18 22:06:43 -0800

    boolean debug

--- web/app/brep/debug/debugger/shellExplorer.jsx
+++ web/app/brep/debug/debugger/shellExplorer.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
+import Section from "./section";
 
 export default class ShellExplorer extends React.PureComponent {
 
   render() {
     let {shell} = this.props;
+
+    let faceControls = <span>
+      <i className='fa fa-fw fa-eye' onClick={alert}/>
+      <i className='fa fa-fw fa-eye-slash' onClick={alert}/>
+    </span>;  
+    
     return <div className='shell-explorer'>
-      <div className='caption'>faces</div>
-      {shell.faces.map(face => <div>
-        <div className='caption'>face {face.refId}</div>
-        {mapIterator(face.edges, e => <div>
-          edge: {e.refId}    
-        </div>)}    
-      </div>)
-      }
+      
+      <Section name='faces' closable>
+        {shell.faces.map(face => <Section name={`face ${face.refId}`} tabs='0.5em' closable defaultClosed={true} controls={faceControls}>
+            {mapIterator(face.edges, e => <div>
+              edge: {e.refId}    
+            </div>)}    
+          </Section>)
+        }
+      </Section>
     </div>;
   }
 }
