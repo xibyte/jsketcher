b040e6882add5e0526b1fd2c15c87f39ca6e812f
xibyte <xibyte@gmail.com>
2018-01-06 15:55:18 -0800

    switch to React

--- web/app/3d/plugins.js
+++ web/app/3d/plugins.js
@@ -1,4 +1,4 @@
-import * as DomPlugin from './domPlugin';
+import * as DomPlugin from './dom/domPlugin';
 import * as PickControlPlugin from './scene/controls/pickControlPlugin';
 import * as ScenePlugin from './scene/scenePlugin';
 import * as SelectionMarkerPlugin from './scene/selectionMarker/selectionMarkerPlugin';
