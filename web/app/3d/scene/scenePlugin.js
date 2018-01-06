956cc753d65cb3896e1076149a0e0b06f2a16f99
Val Erastov <xibyte@gmail.com>
2018-01-05 18:45:05 -0800

    export pick control to a plugin

--- web/app/3d/scene/scenePlugin.js
+++ web/app/3d/scene/scenePlugin.js
@@ -1,20 +1,14 @@
 import Viewer from './viewer';
 import CadScene from "./cadScene";
-import PickControl from "./controls/pickControl";
 
 export function activate(context) {
-  context.bus.subscribe('dom:viewerContainer', (container) => {
-    initScene(context, container);
-  });
-}
-
-function initScene(context, container) {
-  let viewer = new Viewer(container);
+  let {dom} = context.services;
+  
+  let viewer = new Viewer(dom.viewerContainer);
+  
   context.services.viewer = viewer;
-  
   context.services.cadScene = new CadScene(viewer.sceneSetup.rootGroup);
-  
-  let pickControl = new PickControl(context);
 
   context.bus.subscribe('scene:update', () => viewer.render());
 }
+
