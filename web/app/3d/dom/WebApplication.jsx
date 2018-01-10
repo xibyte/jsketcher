ee4036babff5c6f236655a5719e6d90a1c5ecfd3
Val Erastov <xibyte@gmail.com>
2018-01-09 20:00:00 -0800

    clean-up

--- web/app/3d/dom/WebApplication.jsx
+++ web/app/3d/dom/WebApplication.jsx
@@ -4,24 +4,24 @@ export default class WebApplication extends React.Component {
 
   render() {
     return <Fragment>
-        <div className="app-tab-view" id="view-3d">
+      <div className='app-tab-view' id='view-3d'>
         <div style={{
           position: 'relative',
-          width: '100%', 
+          width: '100%',
           height: '100%'
-          }}>
-          <div id="right-panel"></div>
-          <div id="viewer-container"></div>
-          <div id="control-bar">
-            <div className="left-group">
+        }}>
+          <div id='right-panel'/>
+          <div id='viewer-container'/>
+          <div id='control-bar'>
+            <div className='left-group'>
             </div>
-            <div className="right-group">
+            <div className='right-group'>
             </div>
           </div>
         </div>
       </div>
-      <div id="tab-switcher"></div>
-      <a id="downloader" style={{display: 'none'}} ></a>
+      <div id='tab-switcher'/>
+      <a id='downloader' style={{display: 'none'}}/>
     </Fragment>
   }
 }
