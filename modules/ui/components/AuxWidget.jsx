0aae7e995d08f425a1b0f93c6dafd5864edcc583
xibyte <xibyte@gmail.com>
2018-01-17 01:37:22 -0800

    action info popup

--- modules/ui/components/AuxWidget.jsx
+++ modules/ui/components/AuxWidget.jsx
@@ -5,7 +5,7 @@ import ls from './AuxWidget.less';
 import AdjustableAbs from "./AdjustableAbs";
 
 export default function AuxWidget({flatTop, flatBottom, children, className, ...props}) {
-  return <AdjustableAbs className={cx(ls.root, flatTop && ls.flatTop, flatBottom && ls.flatBottom, className)} {...props }>
+  return <AdjustableAbs className={cx(ls.root, flatTop && ls.flatTop, flatBottom && ls.flatBottom, className)} {...props}>
     {children}
   </AdjustableAbs>
 }
---
+++ web/app/cad/dom/components/ActionInfo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+
+export default function ActionInfo({children}) {
+
+  return <AuxWidget>
+    {children}    
+  </AuxWidget>;
+}
+
+
