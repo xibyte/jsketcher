3f178ba0abf5fbfac2fe80b4c5c4ee87e758c795
Val Erastov <xibyte@gmail.com>
2018-01-23 00:27:34 -0800

    fix box operation icon

--- web/app/cad/craft/primitives/boxOperation.js
+++ web/app/cad/craft/primitives/boxOperation.js
@@ -20,7 +20,7 @@ export default {
   id: 'BOX',
   metadata: METADATA,
   label: 'Box',
-  icon: 'img/cad/box',
+  icon: 'img/cad/cube',
   info: 'creates new object box',
   paramsInfo: ({width, height, depth}) => `(${width}, ${height}, ${depth})`,
   previewGeomProvider: ({width, height, depth}) => createBoxGeometry(width, height, depth),
