2bd771a4a3ca51bbe1e66221d29b370a80fa3909
Val Erastov <xibyte@gmail.com>
2018-01-23 01:13:56 -0800

    tpi plugin

--- web/app/utils/jqueryfy.js
+++ web/app/utils/jqueryfy.js
@@ -1,6 +1,6 @@
 import $ from 'jquery'
 
-// Usage of jquery is totally deprecated. It exists only to support legacy code and 
-// will be gone soon after complete transition to React. 
+// Usage of jquery is absolutely deprecated. It exists only to support legacy code(2d sketcher and test framework) and 
+// will be gone soon after complete transition to React. Main application never uses jquery.
 
 window.jQuery = window.$ = $;
