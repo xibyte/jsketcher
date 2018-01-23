4d8d4469068ca82059c7fbaac193465426247cf1
Val Erastov <xibyte@gmail.com>
2018-01-23 00:57:27 -0800

    debug plugin

--- web/app/cad/part/partModellerPlugin.js
+++ web/app/cad/part/partModellerPlugin.js
@@ -1,9 +1,11 @@
 import * as UIConfigPlugin from './uiConfigPlugin';
 import * as PartOperationsPlugin from './partOperationsPlugin';
+import * as DebugPlugin from '../debugPlugin';
 import {activatePlugins} from "../init/startApplication";
 
 const PART_MODELLER_PLUGINS = [
   UIConfigPlugin,
+  DebugPlugin,
   PartOperationsPlugin
 ];
 
