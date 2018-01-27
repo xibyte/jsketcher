a9150356bc88ba47f6380e6750cbdeb1ca36ae6e
Val Erastov <xibyte@gmail.com>
2018-01-26 19:47:52 -0800

    improve action hint behavior

--- modules/bus/index.js
+++ modules/bus/index.js
@@ -75,6 +75,8 @@ export default class Bus {
   }
   
   dispatch(key, data) {
+    console.log('dispatch: ' + key + ' -> ' + JSON.stringify(data));
+
     if (this.lock.has(key)) {
       console.warn('recursive dispatch');
       return
--- web/app/cad/actions/actionButtonBehavior.js
+++ web/app/cad/actions/actionButtonBehavior.js
@@ -4,9 +4,35 @@ export function mapActionBehavior(actionIdProp) {
   return ({dispatch}, props) => {
     const actionId = props[actionIdProp];
     const actionRunToken = ACTION_TOKENS.actionRun(actionId);
+
+    let request = {actionId, x:0, y:0};
+    let canceled = true;
+    let showed = false;
+
+    function updateCoords({pageX, pageY}) {
+      request.x = pageX + 10;
+      request.y = pageY + 10;
+    }
+    
     return {
       onClick: data => dispatch(actionRunToken, data),
-      onMouseEnter: ({pageX, pageY}) => dispatch(ACTION_TOKENS.SHOW_HINT_FOR, [actionId, pageX, pageY]),
-      onMouseLeave: () => dispatch(ACTION_TOKENS.SHOW_HINT_FOR, null)
+      onMouseEnter: e => {
+        updateCoords(e);
+        canceled = false;
+        showed = false;
+        setTimeout(() => {
+          if (!canceled) {
+            showed = true;
+            dispatch(ACTION_TOKENS.SHOW_HINT_FOR, request)
+          }
+        }, 500);
+      },
+      onMouseMove: updateCoords,
+      onMouseLeave: () => {
+        canceled = true;
+        if (showed) {
+          dispatch(ACTION_TOKENS.SHOW_HINT_FOR, null)
+        }
+      }
   }};
 }
