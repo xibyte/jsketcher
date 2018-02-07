54d0bc4ba589ff1eab76068a4c071d64f48d3b12
Val Erastov <xibyte@gmail.com>
2018-02-07 00:50:26 -0800

    changing Button api

--- modules/ui/components/controls/Button.jsx
+++ modules/ui/components/controls/Button.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import ls from './Button.less'
 
-export default function Button({text, type, onClick}) {
+export default function Button({type, onClick, children}) {
 
-  return <button onClick={onClick} className={ls[type]}>{text}</button>
+  return <button onClick={onClick} className={ls[type]}>{children}</button>
 
 }
 
