6d5c218707b7aef3fb7c11c64e659a505009d2a8
Val Erastov <xibyte@gmail.com>
2018-06-17 23:57:47 -0700

    fix fillet info rendering

--- web/app/cad/craft/fillet/filletOperation.js
+++ web/app/cad/craft/fillet/filletOperation.js
@@ -5,7 +5,7 @@ export default {
   label: 'Fillet',
   icon: 'img/cad/intersection',
   info: 'creates a fillet on selected edges',
-  paramsInfo: ({operands}) => operands.map(o => o.thikness).join(' ,'),
+  paramsInfo: ({edges}) => edges.map(o => o.thikness).join(' ,'),
   previewGeomProvider: () => new THREE.Geometry(),
   run: (request) => console.dir(request),
   wizard: FilletWizard,
