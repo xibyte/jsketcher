c9eecb8a6fc0db503c05ef5c2a0983193303e3a4
Val Erastov <xibyte@gmail.com>
2018-08-05 22:54:52 -0700

    fix history wizard

--- web/app/cad/craft/wizard/components/WizardManager.jsx
+++ web/app/cad/craft/wizard/components/WizardManager.jsx
@@ -4,19 +4,20 @@ import HistoryWizard from './HistoryWizard';
 import connect from '../../../../../../modules/ui/connect';
 import decoratorChain from '../../../../../../modules/ui/decoratorChain';
 import mapContext from '../../../../../../modules/ui/mapContext';
-import {finishHistoryEditing} from '../../craftHistoryUtils';
+import {finishHistoryEditing, stepOverridingParams} from '../../craftHistoryUtils';
 import {createPreviewer} from '../../../preview/scenePreviewer';
 import errorBoundary from 'ui/errorBoundary';
 import initializeBySchema from '../../intializeBySchema';
 import validateParams from '../../validateParams';
 import {combine} from 'lstream';
+import {NOOP} from '../../../../../../modules/gems/func';
 
 class WizardManager extends React.Component {
 
   render() {
-    
-    
-    let {insertOperation, registry, close} = this.props;
+
+    let {insertOperation, registry, close, history, pointer, stepHistory, cancelHistoryEdit, 
+      previewerCreator, createValidator, initializeOperation} = this.props;
 
     if (insertOperation) {
       let operation = registry[insertOperation];
@@ -24,20 +25,32 @@ class WizardManager extends React.Component {
         throw 'unknown operation ' + type;
       }
 
-      let params = this.props.initializeOperation(operation);
-      let validator = this.props.createValidator(operation);
-      let closeInstance = close;
+      let params = initializeOperation(operation);
       return <Wizard type={insertOperation}
-              createPreviewer={this.props.previewerCreator(operation)}
+              createPreviewer={previewerCreator(operation)}
               form={operation.form}
               params={params}
-              validate={validator}
-              close={closeInstance}/>
+              validate={createValidator(operation)}
+              close={close}/>
       
     } else {
-      return <HistoryWizard createValidator={this.props.createValidator}
-                            getOperation={this.props.getOperation}
-                            previewerCreator={this.props.previewerCreator}/>;
+
+      if (pointer === history.length - 1) {
+        return null;
+      }
+      
+      let {type, params} = history[pointer + 1];
+      let operation = registry[type];
+      if (!operation) {
+        throw 'unknown operation ' + type;
+      }
+
+      return <Wizard type={type}
+                     validate={createValidator(operation)}
+                     createPreviewer={previewerCreator(operation)}
+                     params={clone(params)}
+                     form={operation.form}
+                     onCancel={cancelHistoryEdit} onOK={stepHistory} close={NOOP} />
     }
   }
 }
@@ -47,12 +60,18 @@ export default decoratorChain(
     streams.wizard,
     streams.craft.modifications,
     streams.operation.registry,
-  ).map(([w, {pointer}, registry]) => ({insertOperation: w&&w.type, pointer, registry}))),
+  ).map(([w, {pointer, history}, registry]) => ({insertOperation: w&&w.type, pointer, registry, history}))),
   mapContext(ctx => ({
     close: ctx.services.wizard.close,
     initializeOperation: operation => initializeBySchema(operation.schema, ctx),
     previewerCreator: operation => () => createPreviewer(operation.previewGeomProvider, ctx.services),
-    createValidator: operatation => params => validateParams(ctx.services, params, operatation.schema)
+    createValidator: operatation => params => validateParams(ctx.services, params, operatation.schema),
+    stepHistory: params => ctx.streams.craft.modifications.update(modifications => stepOverridingParams(modifications, params)),
+    cancelHistoryEdit: () => ctx.streams.craft.modifications.update(modifications => finishHistoryEditing(modifications)),
   }))
 )
 (WizardManager);
+
+function clone(params) {
+  return JSON.parse(JSON.stringify(params));
+}
