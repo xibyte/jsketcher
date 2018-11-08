5f04782f56682abc1e56b84cff4da2e2770f9cf2
Val Erastov <xibyte@gmail.com>
2018-11-07 21:58:36 -0800

    improve ObjectExplorer to show adjacent faces for an edge

--- web/app/cad/craft/ui/ObjectExplorer.jsx
+++ web/app/cad/craft/ui/ObjectExplorer.jsx
@@ -1,44 +1,63 @@
 import React from 'react';
 import connect from 'ui/connect';
 import {Section} from 'ui/components/Section';
-import Fa from '../../../../../modules/ui/components/Fa';
-import {constant} from '../../../../../modules/lstream';
+import Fa from 'ui/components/Fa';
+import {constant} from 'lstream';
 import ls from './ObjectExplorer.less';
 import cx from 'classnames';
 import {MShell} from '../../model/mshell';
 import {MDatum} from '../../model/mdatum';
+import mapContext from 'ui/mapContext';
+import decoratorChain from 'ui/decoratorChain';
 
 export default connect(streams => streams.craft.models.map(models => ({models})))
 (function ObjectExplorer({models}) {
 
-  return models.map(m => (m instanceof MShell) ? <ModelSection type='shell' model={m} defaultOpen={true}>
+  return models.map(m => (m instanceof MShell) ? <ModelSection type='shell' model={m} defaultOpen={true} key={m.id}>
     <Section label='faces' defaultOpen={true}>
       {
-        m.faces.map(f => <ModelSection type='face' model={f}>
-          <Section label={f.sketchObjects.length ? 'sketch' : <span className={ls.hint}>{'<no sketch assigned>'}</span>}>
-            {f.sketchObjects.map(o => <div>{o.id + ':' + o.sketchPrimitive.constructor.name}</div>)}
-          </Section>
-        </ModelSection>)
+        m.faces.map(f => <FaceSection face={f} key={f.id} />)
       }
     </Section>
     <Section label='edges' defaultOpen={true}>
-      {m.edges.map(e => <ModelSection type='edge' model={e} />)}
+      {m.edges.map(e => <EdgeSection edge={e} key={e.id} />)}
     </Section>
 
   </ModelSection> : (m instanceof MDatum) ? <ModelSection type='datum' model={m} defaultOpen={true}/> : null);
 
 });
 
-const ModelSection = connect((streams, props) => (streams.selection[props.type]||constant([])).map(selection => ({selection})))
+function EdgeSection({edge}) {
+  return <ModelSection type='edge' model={edge} key={edge.id}>
+    {edge.adjacentFaces.map(f => <FaceSection face={f} key={f.id}/>)}
+  </ModelSection>
+}
+
+function FaceSection({face}) {
+  return <ModelSection type='face' model={face} key={face.id}>
+    <Section label={face.sketchObjects.length ? 'sketch' : <span className={ls.hint}>{'<no sketch assigned>'}</span>}>
+      {face.sketchObjects.map(o => <div key={o.id}>{o.id + ':' + o.sketchPrimitive.constructor.name}</div>)}
+    </Section>
+    <Section label='edges' defaultOpen={false}>
+      {face.edges.map(e => <EdgeSection edge={e} key={e.id}/>)}
+    </Section>
+  </ModelSection>;
+}
+
+const ModelSection = decoratorChain(
+  mapContext((ctx, props) => ({
+    select: () => ctx.services.selection[props.type].select([props.model.id])
+  })),
+  connect((streams, props) => (streams.selection[props.type] || constant([])).map(selection => ({selection}))))
 (
-  function ModelSection({model, type, selection, ...props}) {
+  function ModelSection({model, type, selection, select, ...props}) {
     let labelClasses = cx(ls.modelLabel, {
       [ls.selected]: selection.indexOf(model.id) !== -1
     });
-    let label = <span className={labelClasses}><CommonControls /> 
-      {type} {model.id}
+    let label = <span className={labelClasses}><CommonControls/>
+      <span onClick={select}>{type} {model.id}</span>
     </span>;
-    return <Section label={label} {...props}/> 
+    return <Section label={label} {...props}/>;
   }
 );
 
