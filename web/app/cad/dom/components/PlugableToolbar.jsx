579ac1203f482bf684f929333ca33d72b7424420
Val Erastov <xibyte@gmail.com>
2018-11-29 23:40:47 -0800

    reorganizing UI layout

--- web/app/cad/dom/components/PlugableToolbar.jsx
+++ web/app/cad/dom/components/PlugableToolbar.jsx
@@ -8,29 +8,37 @@ import {mapActionBehavior} from '../../actions/actionButtonBehavior';
 import capitalize from 'gems/capitalize';
 import decoratorChain from 'ui/decoratorChain';
 import {combine, merger} from 'lstream';
-import mapContext from '../../../../../modules/ui/mapContext';
+import mapContext from 'ui/mapContext';
+import {ToolbarSplitter} from 'ui/components/Toolbar';
 
-function ConfigurableToolbar({actions, small, ...props}) {
-
-  return <Toolbar small={small} {...props}>
-    {actions.map(actionRef => {
-      let [id, overrides] = toIdAndOverrides(actionRef);
-      return <ConnectedActionButton actionId={id} key={id} small={small} {...overrides} />
-    })}
+function ConfigurableToolbar({actions, size, ...props}) {
+  return <Toolbar size={size} {...props}>
+    <ToolbarActionButtons actions={actions} size={size} />
   </Toolbar>
 }
 
-function ActionButton({label, icon96, icon32, cssIcons, symbol, small, enabled, visible, actionId, ...props}) {
+export function ToolbarActionButtons({actions, size}) {
+  return actions.map(actionRef => {
+    if (actionRef === '-') {
+      return <ToolbarSplitter />;
+    }
+    let [id, overrides] = toIdAndOverrides(actionRef);
+    return <ConnectedActionButton actionId={id} key={id} size={size} {...overrides} />
+  });
+}
+
+function ActionButton({label, icon96, icon32, cssIcons, symbol, size, noLabel, enabled, visible, actionId, ...props}) {
   if (!visible) {
     return null;
   }
 
+  let smallOrMedium = size === 'medium' || size === 'small';
   let icon;
-  if (small) {
+  if (smallOrMedium) {
     if (cssIcons) {
       icon = <Fa fa={cssIcons} fw />;  
     } else if (icon32) {
-      icon = <ImgIcon url={icon32} size={16} />;
+      icon = <ImgIcon url={icon32} size={size === 'small' ? 16 : 24} />;
     }
   } else {
     icon = <ImgIcon url={icon96} size={48} />; 
@@ -44,11 +52,11 @@ function ActionButton({label, icon96, icon32, cssIcons, symbol, small, enabled, 
     
   return <ToolbarButton disabled={!enabled} {...props}>
     {icon}
-    {!small && <div>{capitalize(label)}</div>}
+    {!(smallOrMedium || noLabel)&& <div>{capitalize(label)}</div>}
   </ToolbarButton>
 }
 
-const ConnectedActionButton = decoratorChain(
+export const ConnectedActionButton = decoratorChain(
   connect((streams, {actionId})  => combine(streams.action.appearance[actionId], streams.action.state[actionId]).map(merger)),
   mapContext(mapActionBehavior(props => props.actionId))
 )
@@ -60,7 +68,3 @@ export function createPlugableToolbar(streamSelector) {
   )
   (props => <ConfigurableToolbar {...props} />);
 }
-
-export const HeadsUpToolbar = createPlugableToolbar(streams => streams.ui.toolbars.headsUp);
-export const AuxiliaryToolbar = createPlugableToolbar(streams => streams.ui.toolbars.auxiliary);
-
