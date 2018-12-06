ffd09b00a3b28761ccaaf0a4c6c9d06f8f2df33a
Val Erastov <xibyte@gmail.com>
2018-12-05 23:26:15 -0800

    ability to change datums within operations

--- web/app/cad/craft/primitives/box/BoxWizard.jsx
+++ web/app/cad/craft/primitives/box/BoxWizard.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import {Group} from '../../wizard/components/form/Form';
 import {NumberField, ReadOnlyValueField} from '../../wizard/components/form/Fields';
 import BooleanChoice from '../../wizard/components/form/BooleanChioce';
+import EntityList from '../../wizard/components/form/EntityList';
 
 export default function BoxWizard() {
   return <Group>
-    <ReadOnlyValueField name='datum' placeholder='origin'/>
+    <EntityList name='datum' placeholder='origin'/>
     <NumberField name='width' />
     <NumberField name='height' />
     <NumberField name='depth' />
