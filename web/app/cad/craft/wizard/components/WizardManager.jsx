d1382e550af7ef84b203eb698a8042b8a6fc6c45
Val Erastov <xibyte@gmail.com>
2018-12-10 23:14:08 -0800

    localize workingParams to wizard context

--- web/app/cad/craft/wizard/components/WizardManager.jsx
+++ web/app/cad/craft/wizard/components/WizardManager.jsx
@@ -3,23 +3,21 @@ import Wizard from './Wizard';
 import connect from 'ui/connect';
 import decoratorChain from 'ui/decoratorChain';
 import mapContext from 'ui/mapContext';
-import {finishHistoryEditing} from '../../craftHistoryUtils';
 
-function WizardManager({type, changingHistory, resolve, cancel, stepHistory, insertOperation, cancelHistoryEdit, applyWorkingRequest}) {
-  if (!type) {
+function WizardManager({wizardContext, type, changingHistory, cancel, cancelHistoryEdit, applyWorkingRequest}) {
+  if (!wizardContext) {
     return null;
   }
-  return <Wizard resolveOperation={resolve}
+  return <Wizard key={wizardContext.ID}
+                 context={wizardContext}
                  onCancel={changingHistory ? cancelHistoryEdit : cancel}
                  onOK={applyWorkingRequest} />
 }
 
 export default decoratorChain(
-  connect(streams => streams.wizard.effectiveOperation),
-  mapContext((ctx, props) => ({
+  connect(streams => streams.wizard.wizardContext.map(wizardContext => ({wizardContext}))),
+  mapContext(ctx => ({
     cancel: ctx.services.wizard.cancel,
-    resolve: type => ctx.services.operation.get(type),
-    cancelHistoryEdit: () => ctx.streams.craft.modifications.update(modifications => finishHistoryEditing(modifications)),
     applyWorkingRequest: ctx.services.wizard.applyWorkingRequest 
   }))
 )
