22b2916049be1adc41d1fc728c44e26d14f9f923
Val Erastov <xibyte@gmail.com>
2018-12-15 00:30:26 -0800

    previewer for revolve operation

--- web/app/cad/preview/previewPlugin.js
+++ web/app/cad/preview/previewPlugin.js
@@ -12,17 +12,26 @@ export function activate(ctx) {
       let previewer = null;
       materializedWorkingRequest$.attach(({type, params}) => {
         if (previewer === null) {
-          if (operation.previewGeomProvider) {
-            previewer = createPreviewer(operation.previewGeomProvider, services, params);
-          } else if (operation.previewer) {
-            previewer = operation.previewer(ctx, params, wizCtx.updateParams);
+          try {
+            if (operation.previewGeomProvider) {
+              previewer = createPreviewer(operation.previewGeomProvider, services, params);
+            } else if (operation.previewer) {
+              previewer = operation.previewer(ctx, params, wizCtx.updateParams);
+            }
+          } catch (e) {
+            console.error(e);
+            return;
           }
           wizCtx.addDisposer(() => {
             previewer.dispose();
             ctx.services.viewer.requestRender();
           });
         } else {
-          previewer.update(params);
+          try {
+            previewer.update(params);
+          } catch (e) {
+            console.error(e);
+          }
         }
         ctx.services.viewer.requestRender();
       });
