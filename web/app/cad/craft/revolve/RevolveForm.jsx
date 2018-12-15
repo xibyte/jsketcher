be61121af4d0f08c60ffb587551a836560235d9c
Val Erastov <xibyte@gmail.com>
2018-12-15 01:40:48 -0800

    add all boolean operations to revolve operation

--- web/app/cad/craft/revolve/RevolveForm.jsx
+++ web/app/cad/craft/revolve/RevolveForm.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {CheckboxField, NumberField} from '../wizard/components/form/Fields';
 import {Group} from '../wizard/components/form/Form';
 import Entity from '../wizard/components/form/Entity';
+import BooleanChoice from '../wizard/components/form/BooleanChioce';
 
 export default function RevolveForm() {
 
@@ -9,6 +10,6 @@ export default function RevolveForm() {
     <NumberField name='angle' />
     <Entity name='face' entity='face' />
     <Entity name='axis' entity='sketchObject' />
-    <CheckboxField name='cut' />
+    <BooleanChoice name='boolean' />
   </Group>;
 }
