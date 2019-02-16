b51361fa3cc00ff256d14e18c56767b424ffb898
Val Erastov <xibyte@gmail.com>
2019-02-15 20:18:47 -0800

    extrude test cases

--- web/test/utils/sketcher-utils.js
+++ web/test/utils/sketcher-utils.js
@@ -26,12 +26,29 @@ export function clickXY(app, x, y, attrs) {
   app.viewer.toolManager.tool.mouseup(new TestMouseEvent(x, y, 'mouseup', attrs));
 }
 
+export function moveAndClickXY(app, x, y, attrs) {
+  app.viewer.toolManager.tool.mousemove(new TestMouseEvent(x, y, 'mousedown', attrs));
+  clickXY(app, x, y, attrs);
+}
+
 export function move(app, from, to) {
   const toolManager = app.viewer.toolManager;
   toolManager.tool.mousedown(new TestMouseEvent(from.x, from.y));
   toolManager.tool.mousemove(new TestMouseEvent(to.x, to.y));
   toolManager.tool.mouseup(new TestMouseEvent(to.x, to.y));
 }
+
+export function moveInModel(app, fromX, fromY, toX, toY) {
+  const toolManager = app.viewer.toolManager;
+  [fromX, fromY] = modelToScreen(app.viewer, fromX, fromY);
+  [toX, toY] = modelToScreen(app.viewer, toX, toY);
+
+  toolManager.tool.mousemove(new TestMouseEvent(fromX, fromY));
+  toolManager.tool.mousedown(new TestMouseEvent(fromX, fromY));
+  toolManager.tool.mousemove(new TestMouseEvent(toX, toY));
+  toolManager.tool.mouseup(new TestMouseEvent(toX, toY));
+}
+
 
 export function addSegment(app, aX, aY, bX, bY) {
   app.actions['addSegment'].action();
@@ -52,6 +69,70 @@ export function addSegmentInModel(app, aX, aY, bX, bY) {
   [bX, bY] = modelToScreen(app.viewer, bX, bY);
 
   return addSegment(app, aX, aY, bX, bY);
+}
+
+export function addArc(app, cX, cY, aX, aY, bX, bY) {
+
+  [aX, aY] = modelToScreen(app.viewer, aX, aY);
+  [bX, bY] = modelToScreen(app.viewer, bX, bY);
+  [cX, cY] = modelToScreen(app.viewer, cX, cY);
+
+  app.actions['addArc'].action();
+
+  moveAndClickXY(app, cX, cY);
+  moveAndClickXY(app, aX, aY);
+  let arc = app.viewer.toolManager.tool.arc;
+  moveAndClickXY(app, bX, bY);
+  app.viewer.toolManager.releaseControl();
+  return arc;
+}
+
+export function addCircle(app, cX, cY, R) {
+  let [rX, rY] = modelToScreen(app.viewer, cX + R, cY);
+  [cX, cY] = modelToScreen(app.viewer, cX, cY);
+  app.actions['addCircle'].action();
+  moveAndClickXY(app, cX, cY);
+  let circle = app.viewer.toolManager.tool.circle;
+  moveAndClickXY(app, rX, rY);
+  app.viewer.toolManager.releaseControl();
+  return circle;
+}
+
+export function addEllipse(app, aX, aY, bX, bY, rX, rY) {
+  [aX, aY] = modelToScreen(app.viewer, aX, aY);
+  [bX, bY] = modelToScreen(app.viewer, bX, bY);
+  [rX, rY] = modelToScreen(app.viewer, rX, rY);
+  app.actions['addEllipse'].action();
+  moveAndClickXY(app, aX, aY);
+  let ellipse = app.viewer.toolManager.tool.ellipse;
+  moveAndClickXY(app, bX, bY);
+  moveAndClickXY(app, rX, rY);
+  app.viewer.toolManager.releaseControl();
+  return ellipse;
+}
+
+export function addEllipticalArc(app, aX, aY, bX, bY, rX, rY) {
+  [aX, aY] = modelToScreen(app.viewer, aX, aY);
+  [bX, bY] = modelToScreen(app.viewer, bX, bY);
+  [rX, rY] = modelToScreen(app.viewer, rX, rY);
+  app.actions['addEllipticalArc'].action();
+  moveAndClickXY(app, aX, aY);
+  let ellipse = app.viewer.toolManager.tool.ellipse;
+  moveAndClickXY(app, bX, bY);
+  moveAndClickXY(app, rX, rY);
+  app.viewer.toolManager.releaseControl();
+  return ellipse;
+}
+
+export function addBezier(app, aX, aY, bX, bY) {
+  [aX, aY] = modelToScreen(app.viewer, aX, aY);
+  [bX, bY] = modelToScreen(app.viewer, bX, bY);
+  app.actions['addBezierCurve'].action();
+  moveAndClickXY(app, aX, aY);
+  let curve = app.viewer.toolManager.tool.curve;
+  moveAndClickXY(app, bX, bY);
+  app.viewer.toolManager.releaseControl();
+  return curve;
 }
 
 export function polyLine(app) {
