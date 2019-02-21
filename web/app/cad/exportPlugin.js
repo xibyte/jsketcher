9d28160bf49acb0010f2f49ebd62f624ae2d35c2
Val Erastov <xibyte@gmail.com>
2019-02-21 02:08:52 -0800

    adding some icons for datum operations

--- web/app/cad/exportPlugin.js
+++ web/app/cad/exportPlugin.js
@@ -9,7 +9,28 @@ export function activate(ctx) {
     exportTextData(result, ctx.services.project.id + ".stl");
   }
   
+  function imagePng() {
+    let auxVisible = ctx.services.cadScene.auxGroup.visible;
+    ctx.services.cadScene.auxGroup.visible = false;
+    let renderer = ctx.services.viewer.sceneSetup.renderer;
+    let clearAlpha = renderer.getClearAlpha();
+    renderer.setClearAlpha(0);
+    renderer.preserveDrawingBuffer = true;
+    ctx.services.viewer.sceneSetup.render();
+
+    let link = document.getElementById("downloader");
+    link.href = renderer.domElement.toDataURL('image/png');
+    link.download = ctx.services.project.id + "-snapshot.png";
+    link.click();
+
+    renderer.preserveDrawingBuffer = false;
+    ctx.services.cadScene.auxGroup.visible = auxVisible;
+    renderer.setClearAlpha(0);
+    renderer.setClearAlpha(clearAlpha);
+    ctx.services.viewer.sceneSetup.render();
+  } 
+  
   ctx.services.export = {
-    stlAscii
+    stlAscii, imagePng
   };
 }
