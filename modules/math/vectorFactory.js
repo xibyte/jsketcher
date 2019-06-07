ae586c30cd7ca5ef2609c8858440023836db76bf
Val Erastov <xibyte@gmail.com>
2019-06-07 11:32:14 -0700

    merge close vectors when reading sketch

---
+++ modules/math/vectorFactory.js
@@ -0,0 +1,35 @@
+import {veqXYZ} from '../../web/app/brep/geom/tolerance';
+import Vector from './vector';
+
+export default class VectorFactory {
+
+  constructor(tolerance) {
+    this.vectors = [];
+  }
+
+  addVertices(vertices) {
+    for (let v of vertices) {
+      this.vectors.push(v);
+    }
+  }
+
+  find(x, y, z) {
+    for (let v of this.vectors) {
+      if (veqXYZ(v.x, v.y, v.z, x, y, z)) {
+        return v;
+      }
+    }
+    return null;
+  }
+
+  create(x, y, z, onExistent) {
+    let vector = this.find(x, y, z);
+    if (vector === null) {
+      vector = new Vector(x, y, z);
+      this.vectors.push(vector);
+    } else if (onExistent !== undefined) {
+      return onExistent(vector);
+    }
+    return vector;
+  }
+}
