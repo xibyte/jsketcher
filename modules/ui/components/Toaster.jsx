e20b7e892f38b98300192940bd8e76725f6016dd
Val Erastov (xibyte) <xibyte@gmail.com>
2020-03-05 00:33:00 -0800

    constraint action descriptions

---
+++ modules/ui/components/Toaster.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+
+export function Toaster({}) {
+
+  return <div>
+
+  </div>;
+
+}
--- web/app/sketcher/components/SketcherApp.jsx
+++ web/app/sketcher/components/SketcherApp.jsx
@@ -4,12 +4,15 @@ import {ConstraintEditor} from './ConstraintEditor';
 import {ContextualControls} from './ContextualControls';
 import {ConstraintList} from './ConstraintExplorer';
 import {StreamsContext} from 'ui/streamsContext';
+import {ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 export const SketcherAppContext = React.createContext({});
 
 export function SketcherApp({applicationContext}) {
   return <SketcherAppContext.Provider value={applicationContext}>
     <StreamsContext.Provider value={applicationContext}>
+      <ToastContainer />
       <RightSideControls />
       {ReactDOM.createPortal(
         <ConstraintList />,
