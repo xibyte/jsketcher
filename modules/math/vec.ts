0f5e395f4d1c47e554c65fea0d5e5c6dfa7dc394
Val Erastov (xibyte) <xibyte@gmail.com>
2020-07-19 13:14:38 -0700

    organizing math module - enforcing cardinality for vector data

--- modules/math/vec.ts
+++ modules/math/vec.ts
@@ -4,83 +4,83 @@ type UnaryFn = (number) => number;
 type BinaryFn = (n1: number, n2: number) => number;
 export type VectorData = number[];
 
-export function scalarOperand(v: VectorData, out: VectorData, func: UnaryFn) {
+export function scalarOperand<T extends VectorData>(v: T, out: T, func: UnaryFn) {
   for (let i = 0; i < v.length; i++) {
     out[i] = func(v[i]);
   }
   return out;
 }
 
-function vectorOperand(v1: VectorData, v2: VectorData, out: VectorData, func: BinaryFn): VectorData {
+function vectorOperand<T extends VectorData>(v1: T, v2: T, out: T, func: BinaryFn): T {
   for (let i = 0; i < v1.length; i++) {
     out[i] = func(v1[i], v2[i]);
   }
   return out;
 }
 
-export function __mul(v: VectorData, scalar: number, out: VectorData): VectorData {
+export function __mul<T extends VectorData>(v: T, scalar: number, out: T): T {
   return scalarOperand(v, out, x => x * scalar); 
 }
 
-export function _mul(v: VectorData, scalar: number): VectorData {
+export function _mul<T extends VectorData>(v: T, scalar: number): T {
   return __mul(v, scalar, v);
 }
 
-export function mul(v: VectorData, scalar: number): VectorData {
-  return __mul(v, scalar, []);
+export function mul<T extends VectorData>(v: T, scalar: number): T {
+  return __mul(v, scalar, create(v.length) as T);
 }
 
-export function __div(v: VectorData, scalar: number, out: VectorData): VectorData {
+export function __div<T extends VectorData>(v: T, scalar: number, out: T): T {
   return scalarOperand(v, out, x => x / scalar);
 }
 
-export function _div(v: VectorData, scalar: number): VectorData {
+export function _div<T extends VectorData>(v: T, scalar: number): T {
   return __div(v, scalar, v);
 }
 
-export function div(v: VectorData, scalar: number): VectorData {
-  return __div(v, scalar, []);
+export function div<T extends VectorData>(v: T, scalar: number): T {
+  return __div(v, scalar, create(v.length) as T);
 }
 
 
-export function __add(v1: VectorData, v2: VectorData, out: VectorData): VectorData {
+export function __add<T extends VectorData>(v1: T, v2: T, out: T): T {
   return vectorOperand(v1, v2, out, (x1, x2) => x1 + x2);
 }
 
-export function _add(v1: VectorData, v2: VectorData): VectorData {
+export function _add<T extends VectorData>(v1: T, v2: T): T {
   return __add(v1, v2, v1);
 }
 
-export function add(v1: VectorData, v2: VectorData): VectorData {
-  return __add(v1, v2, []);
+export function add<T extends VectorData>(v1: T, v2: T): T {
+  return __add(v1, v2, create(v1.length) as T);
 }
 
-export function __sub(v1: VectorData, v2: VectorData, out: VectorData): VectorData {
+export function __sub<T extends VectorData>(v1: T, v2: T, out: T): T {
   return vectorOperand(v1, v2, out, (x1, x2) => x1 - x2);
 }
 
-export function _sub(v1: VectorData, v2: VectorData): VectorData {
+export function _sub<T extends VectorData>(v1: T, v2: T): T {
   return __sub(v1, v2, v1);
 }
 
-export function sub(v1: VectorData, v2: VectorData): VectorData {
-  return __sub(v1, v2, []);
+export function sub<T extends VectorData>(v1: T, v2: T): T {
+  return __sub(v1, v2, create(v1.length) as T);
 }
 
-export function __negate(v: VectorData, out: VectorData): VectorData {
+export function __negate<T extends VectorData>(v: T, out: T): T {
   return scalarOperand(v, out, x => -x);
 }
 
-export function _negate(v: VectorData): VectorData {
+export function _negate<T extends VectorData>(v: T): T {
   return __negate(v, v);
 }
 
-export function negate(v: VectorData): VectorData {
-  return __negate(v, []);
+export function negate<T extends VectorData>(v: T): T {
+  return __negate(v, create(v.length) as T);
 }
 
 
-export function dot(v1: VectorData, v2: VectorData): number {
+export function dot<T extends VectorData>(v1: T, v2: T): number {
   let sum = 0;
   for (let i = 0; i < v1.length; i++) {
     sum += v1[i] * v2[i];
@@ -88,18 +88,18 @@ export function dot(v1: VectorData, v2: VectorData): number {
   return sum;
 }
 
-export function __cross(v1: VectorData, v2: VectorData, out: VectorData): VectorData {
+export function __cross<T extends VectorData>(v1: T, v2: T, out: T): T {
   out[0] = v1[1] * v2[2] - v1[2] * v2[1];
   out[1] = v1[2] * v2[0] - v1[0] * v2[2];
   out[2] = v1[0] * v2[1] - v1[1] * v2[0];
   return out;
 }
 
-export function cross(v1: VectorData, v2: VectorData): VectorData {
-  return __cross(v1, v2, []);
+export function cross<T extends VectorData>(v1: T, v2: T): T {
+  return __cross(v1, v2, create(v1.length) as T);
 }
 
-export function __normalize(v: VectorData, out: VectorData): VectorData {
+export function __normalize<T extends VectorData>(v: T, out: T): T {
   const mag = length(v);
   if (mag === 0.0) {
     out[0] = out[1] = out[2] = 0;
@@ -107,16 +107,16 @@ export function __normalize(v: VectorData, out: VectorData): VectorData {
   return __div(v, mag, out)
 }
 
-export function cross2d(v1: VectorData, v2: VectorData): number {
+export function cross2d<T extends VectorData>(v1: T, v2: T): number {
   return v1[0] * v2[1] - v1[1] * v2[0];
 }
 
-export function _normalize(v: VectorData): VectorData {
+export function _normalize<T extends VectorData>(v: T): T {
   return __normalize(v, v);
 }
 
-export function normalize(v: VectorData): VectorData {
-  return __normalize(v, []);
+export function normalize<T extends VectorData>(v: T): VectorData {
+  return __normalize(v, create(v.length) as T);
 }
 
 export function lengthSq(v: VectorData): number {
@@ -127,22 +127,20 @@ export function length(v: VectorData): number {
   return Math.sqrt(lengthSq(v));
 }
 
-export function copy(to: VectorData, from: VectorData): VectorData {
+export function copy<T extends VectorData>(to: T, from: T): T {
   for (let i = 0; i < from.length; i++) {
     to[i] = from[i];
   }
   return to;
 }
 
-export function clone(v: VectorData): VectorData {
-  return copy(create(v.length), v);
+export function clone<T extends VectorData>(v: T): T {
+  return copy(create(v.length) as T, v);
 }
 
 export function create(dim: number): VectorData {
-  let out = [];
-  for (let i = 0; i < dim; i++) {
-    out[i] = 0;
-  }
+  const out = new Array<number>(dim);
+  out.fill(0);
   return out;
 }
 
@@ -150,7 +148,7 @@ export {create as newVector};
 
 const sq = v => v * v; 
 
-export function distanceSq(v1: VectorData, v2: VectorData): number {
+export function distanceSq<T extends VectorData>(v1: T, v2: T): number {
   let dSq = 0;
   for (let i = 0; i < v1.length; i++) {
     dSq += sq(v1[i] - v2[i]);
@@ -158,25 +156,25 @@ export function distanceSq(v1: VectorData, v2: VectorData): number {
   return dSq;
 }
 
-export function distance(v1: VectorData, v2: VectorData): number {
+export function distance<T extends VectorData>(v1: T, v2: T): number {
   return Math.sqrt(distanceSq(v1, v2));
 }
 
-export function perp2d(v: VectorData): VectorData {
-  return __perp2d(v, []);
+export function perp2d<T extends VectorData>(v: T): T {
+  return __perp2d(v, [] as T);
 }
 
-export function _perp2d(v: VectorData): VectorData {
+export function _perp2d<T extends VectorData>(v: T): T {
   return __perp2d(v, v);
 }
 
-export function __perp2d([x, y]: VectorData, out: VectorData): VectorData {
+export function __perp2d<T extends VectorData>([x, y]: T, out: T): T {
   out[0] = -y;
   out[1] = x;
   return out;
 }
 
-export function normal3(ccwSequence: [VectorData, VectorData, VectorData]): VectorData {
+export function normal3<T extends VectorData>(ccwSequence: [T, T, T]): T {
   let a = ccwSequence[0];
   let b = ccwSequence[1];
   let c = ccwSequence[2];
