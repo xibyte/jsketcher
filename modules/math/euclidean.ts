08802f29c803ef2c4f8ef01fe8b071e7dd8e965d
Val Erastov (xibyte) <xibyte@gmail.com>
2020-07-19 22:01:26 -0700

    move convex hull to eucleadian module

--- modules/math/euclidean.ts
+++ modules/math/euclidean.ts
@@ -202,3 +202,30 @@ export function lineLineIntersection(p1, p2, v1, v2) {
     u2: n1.dot(p1.minus(p2)) / n1.dot(v2),
   }
 }
+
+export function ConvexHull2D(points) {
+
+  function removeMiddle(a, b, c) {
+    var cross = (a.x - b.x) * (c.y - b.y) - (a.y - b.y) * (c.x - b.x);
+    var dot = (a.x - b.x) * (c.x - b.x) + (a.y - b.y) * (c.y - b.y);
+    return cross < 0 || cross == 0 && dot <= 0;
+  }
+
+  points.sort(function (a, b) {
+    return a.x !== b.x ? a.x - b.x : a.y - b.y;
+  });
+
+  const n = points.length;
+  const hull = [];
+
+  for (let i = 0; i < 2 * n; i++) {
+    const j = i < n ? i : 2 * n - 1 - i;
+    while (hull.length >= 2 && removeMiddle(hull[hull.length - 2], hull[hull.length - 1], points[j])) {
+      hull.pop();
+    }
+    hull.push(points[j]);
+  }
+  hull.pop();
+  return hull;
+}
+
