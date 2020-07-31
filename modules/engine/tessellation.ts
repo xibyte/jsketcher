56d8b85170f2770732fbe864dbc52db295dda8e7
Val Erastov (xibyte) <xibyte@gmail.com>
2020-07-31 01:37:49 -0700

    tessellation API

--- modules/engine/tessellation.ts
+++ modules/engine/tessellation.ts
@@ -4,3 +4,15 @@ export type Tessellation2DNode<T> = [T,T,T]; //just a triangle
 
 export type Tessellation2D<T> = Tessellation2DNode<T>[];
 
+export interface FaceTessellation {
+  positions: number[];
+  indices: number[];
+  normals: number[];
+  uvs: number[];
+}
+
+export interface EdgeTessellation {
+  positions: number[];
+  normals: number[];
+  uvs: number[];
+}
