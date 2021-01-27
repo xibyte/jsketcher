4c67b7118e09ed909661a74f1f7c71abbcf655ef
Val Erastov <xibyte@gmail.com>
2021-01-26 20:23:57 -0800

    defeature by edge

--- modules/brep/operations/directMod/defeaturing.ts
+++ modules/brep/operations/directMod/defeaturing.ts
@@ -1,10 +1,36 @@
 import {Shell} from "brep/topo/shell";
 import {Vertex} from "brep/topo/vertex";
 import {EdgeGraph} from "brep/operations/boolean";
-import {HalfEdge} from "brep/topo/edge";
+import {Edge, HalfEdge} from "brep/topo/edge";
 import {Plane} from "geom/impl/plane";
-import {DEFLECTION} from "../../../../web/app/cad/craft/e0/common";
+import {DEFLECTION, E0_TOLERANCE} from "../../../../web/app/cad/craft/e0/common";
 import {EngineAPI_V1, GenericResponse} from "engine/api";
+
+export function defeatureByEdge(shell: Shell, edge: Edge, engine: EngineAPI_V1): GenericResponse {
+
+  const graph = new EdgeGraph();
+  for (let e of shell.edges) {
+    graph.add(e.halfEdge1);
+    graph.add(e.halfEdge2);
+  }
+
+  const notSourceEdge = (he: HalfEdge) => he.edge !== edge;
+
+  const edges1: HalfEdge[] = graph.vertexToEdge.get(edge.halfEdge1.vertexA);
+  const [a, b] = edges1.filter(notSourceEdge).map(e => e.vertexB.point);
+
+  const edges2: HalfEdge[] = graph.vertexToEdge.get(edge.halfEdge1.vertexB);
+  const [c] = edges2.filter(notSourceEdge).map(e => e.vertexB.point);
+
+  let plane = Plane.by3Points(a, b, c);
+
+  if (plane.normal.multiply(plane.w).minus(edge.halfEdge1.vertexA.point).dot(plane.normal) > 1) {
+    plane = plane.invert();
+  }
+  plane.w += E0_TOLERANCE;
+
+  return cutOff(shell, plane, engine);
+}
 
 export function defeatureByVertex(shell: Shell, vertex: Vertex, engine: EngineAPI_V1): GenericResponse {
 
@@ -21,6 +47,12 @@ export function defeatureByVertex(shell: Shell, vertex: Vertex, engine: EngineAP
   if (plane.normal.multiply(plane.w).minus(vertex.point).dot(plane.normal) > 1) {
     plane = plane.invert();
   }
+
+  return cutOff(shell, plane, engine);
+}
+
+
+function cutOff(shell: Shell, plane: Plane, engine: EngineAPI_V1) {
 
   return engine.splitByPlane({
     deflection: DEFLECTION,
