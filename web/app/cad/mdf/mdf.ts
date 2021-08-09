f0e9543fdc239e21a76c3bbe5a74c33ce762eb57
Val Erastov <xibyte@gmail.com>
2021-08-09 01:18:58 -0700

    mdf prototype / extrude exampl

---
+++ web/app/cad/mdf/mdf.ts
@@ -0,0 +1,61 @@
+import { CoreContext } from "context";
+import { IconType } from "react-icons";
+import { OperationResult } from "../craft/craftPlugin";
+import { OperationDescriptor } from "../craft/operationPlugin";
+import { generateForm } from "./generateForm";
+
+
+interface MDFCommand<R> {
+  id: string;
+  label: string;
+  info: string;
+  icon: IconType | string;
+  run: (request: R, opContext: CoreContext) => OperationResult | Promise<OperationResult>;
+  paramsInfo: (params: R) => string,
+  schema: OperationSchema,
+  mutualExclusiveFields?: string[]
+}
+
+export type Coercable = any;
+
+export type OperationSchema = {
+  [key: string]: SchemaField
+};
+
+export interface SchemaField {
+  type: 'number' | 'face' | 'datumAxis' | 'edge' | 'sketchObject' | 'boolean'
+  defaultValue: Coercable,
+  optional: boolean,
+  label?: string
+}
+
+export function loadMDFCommand<R>(mdfCommand: MDFCommand<R>): OperationDescriptor<R> {
+  return {
+    id: mdfCommand.id,
+    label: mdfCommand.label,
+    icon: mdfCommand.icon,
+    info: mdfCommand.info,
+    paramsInfo: mdfCommand.paramsInfo,
+    onParamsUpdate: (params, name, value) => {
+      if (mdfCommand.mutualExclusiveFields) {
+        handleMutualExclusiveFields(mdfCommand.mutualExclusiveFields, params, name, value);
+      }
+    },
+    run: mdfCommand.run,
+    // actionParams: {
+    //   ...requiresFaceSelection(1)
+    // },
+    form: generateForm(mdfCommand.schema),
+    schema: mdfCommand.schema 
+  }
+}
+
+export function handleMutualExclusiveFields(mutualExclusiveFields, params, name, value) {
+  if (mutualExclusiveFields.includes(name)) {
+    mutualExclusiveFields.forEach(param => {
+      if (param !== name) {
+        delete params[param];
+      }
+    })
+  }
+}
