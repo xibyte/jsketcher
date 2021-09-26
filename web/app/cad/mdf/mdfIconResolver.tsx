c107f6148f789c6217f24ed8383d77b93ddf5be4
Val Erastov <xibyte@gmail.com>
2021-09-26 02:10:50 -0700

    propogate size in px to react icons

--- web/app/cad/mdf/mdfIconResolver.tsx
+++ web/app/cad/mdf/mdfIconResolver.tsx
@@ -1,4 +1,4 @@
-import { DeclaredIcon } from 'cad/icons/DeclarativeIcon';
+import { DeclaredIcon, getSizeInPx } from 'cad/icons/DeclarativeIcon';
 import { IconDeclaration } from 'cad/icons/IconDeclaration';
 import React from 'react';
 import { AiOutlineQuestion } from 'react-icons/ai';
@@ -9,7 +9,7 @@ export function resolveMDFIcon(iconDef: IconDeclaration | IconType) {
         return (props) => <DeclaredIcon {...iconDef} {...props}/>
     } else {
         if (!iconDef || typeof(iconDef) !== 'object') {
-            return AiOutlineQuestion;
+            return size => <AiOutlineQuestion size={getSizeInPx(size)}/>;
         }        
         return iconDef;
     }
