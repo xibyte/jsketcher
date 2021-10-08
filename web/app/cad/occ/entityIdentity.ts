705b86cddec54695bfc23aee2d4e6274e7c211b4
xibyte <xibyte@gmail.com>
2021-10-07 19:09:46 -0700

    edges and normals using opencascade.js

---
+++ web/app/cad/occ/entityIdentity.ts
@@ -0,0 +1,36 @@
+
+import {Shell} from "brep/topo/shell";
+
+export function assignEntityIdentity(created: Shell[], consumed: Shell[]) {
+  
+  let refIndex = indexFacesByRef(consumed);
+
+  let shell = readBrep(data);
+  for (let face of shell.faces) {
+    let ref = getRef(face);
+    if (ref !== undefined) {
+      let consumedFace = refIndex.get(ref);
+      if (consumedFace) {
+        face.data.id = consumedFace.id; 
+      }
+    }
+  }
+  return new MBrepShell(shell, csys);  
+}
+
+function indexFacesByRef(shells: Shell[]) {
+  let index = new Map();
+  if (shells) {
+    for (let shell of shells) {
+      for (let face of shell.faces) {
+        let ref = getRef(face.brepFace);
+        if (ref !== undefined) {
+          index.set(ref, face);
+        }
+      }
+    }
+  }
+  return index;
+}
+
+const getRef = brepFace => brepFace && brepFace.data.externals && brepFace.data.externals.ref;
