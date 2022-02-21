6e2e0667287567aadde2c3149465f4fae6e330f6
Mike Molinari <mike@smbisoft.com>
2022-02-21 02:30:13 +0000

    Updated primtive sphere to use new widgets

--- modules/workbenches/modeler/features/primitive_sphere/index.ts
+++ modules/workbenches/modeler/features/primitive_sphere/index.ts
@@ -1,59 +1,64 @@
 import { ApplicationContext } from 'context';
-import { MBrepShell } from 'cad/model/mshell';
 import { roundValueForPresentation as r } from 'cad/craft/operationHelper';
-import { occ2brep } from 'cad/occ/occ2models';
-import icon32 from './icon32.png';
-import icon96 from './icon96.png';
-
-export default {
-    id: 'primitive_sphere',
-    label: 'primitive_sphere',
-    icon: {
-        iconSet: {
-            medium: {
-                iconType: 'image',
-                iconContent: icon32
-            },
-            large: {
-                iconType: 'image',
-                iconContent: icon96
-            }
-        },
-    },
-    info: 'primitive_sphere',
-    mutualExclusiveFields: [],
-    paramsInfo: ({ diameter }) => `(${r(diameter)} )`,
-    run: ({ diameter, }, ctx: ApplicationContext) => {
-
-        const oc = ctx.occService.occContext;
-
-        const myLocation = new oc.gp_Pnt_3(0, 0, 0);
-        const sphereCenterline = oc.gp.DZ();
-        const sphereOrientationAndLocation = new oc.gp_Ax2_3(myLocation, sphereCenterline);
+import { EntityKind } from "cad/model/entities";
+import { BooleanDefinition } from "cad/craft/schema/common/BooleanDefinition";
+import { OperationDescriptor } from "cad/craft/operationPlugin";
 
 
-        let myBody = new oc.BRepPrimAPI_MakeSphere_1(diameter);
-        //let myBody = new oc.BRepPrimAPI_Make
-
-        const aRes = new oc.TopoDS_Compound();
-        const aBuilder = new oc.BRep_Builder();
-        aBuilder.MakeCompound(aRes);
-        aBuilder.Add(aRes, myBody.Shape());
-
-
-
-        const mobject = new MBrepShell(occ2brep(aRes, ctx.occService.occContext));
-        return {
-            consumed: [],
-            created: [mobject]
-        };
-    },
-    schema: {
-        diameter: {
-            type: 'number',
-            defaultValue: 200,
-            label: 'diameter'
-        },
-    }
+interface PrimitiveSphereParams {
+    radius: number,
+    locations: {},
+    boolean: BooleanDefinition,
 }
 
+const PrimitiveSphereOperation: OperationDescriptor<PrimitiveSphereParams> = {
+    id: 'primitive_sphere',
+    label: 'Primitive Sphere',
+    icon: 'img/cad/sphere',
+    info: 'Primitive Sphere',
+    paramsInfo: ({ radius, }) => `(${r(radius)}  )`,
+    form: [
+        {
+            type: 'number',
+            label: 'Radius',
+            name: 'radius',
+            defaultValue: 50,
+        },
+
+        {
+            type: 'selection',
+            name: 'locations',
+            capture: [EntityKind.DATUM],
+            label: 'locations',
+            multi: false,
+            optional: true,
+            defaultValue: {
+                usePreselection: true,
+                preselectionIndex: 0
+            },
+        },
+
+        {
+            type: 'boolean',
+            name: 'boolean',
+            label: 'boolean',
+            optional: true,
+        }
+
+    ],
+
+
+    run: (params: PrimitiveSphereParams, ctx: ApplicationContext) => {
+
+        let occ = ctx.occService;
+        const oci = occ.commandInterface;
+
+        //pSphere cy 5 10
+        oci.psphere("Sphere", params.radius);
+
+        return occ.utils.applyBooleanModifier(["Sphere"], params.boolean);
+
+    },
+}
+
+export default PrimitiveSphereOperation;
