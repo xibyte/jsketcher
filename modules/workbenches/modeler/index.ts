4973879cd3c83f24fa6c875f889a57901cec631b
Mike Molinari <mike@smbisoft.com>
2022-02-22 04:13:30 +0000

    loft function non working first cut.

--- modules/workbenches/modeler/index.ts
+++ modules/workbenches/modeler/index.ts
@@ -9,6 +9,7 @@ import hole_tool from './features/hole_tool';
 import fillet_tool from './features/fillet_tool';
 import boolean_tool from './features/boolean_tool/boolean.operation';
 import ShellOperation from './features/shell_tool/shell.operation';
+import loft from './features/loft/loft.operation'
 
 export default {
 
@@ -25,5 +26,6 @@ export default {
         RevolveOperation,
         boolean_tool,
         ShellOperation,
+        loft,
     ]
 }
