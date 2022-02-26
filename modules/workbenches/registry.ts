12f4d5761e89e4b291effebcb28967e3d53d4d7f
Mike Molinari <mike@smbisoft.com>
2022-02-26 03:43:21 +0000

    Added fully working mirror body and added start of sheet metal commands in inwork state

--- modules/workbenches/registry.ts
+++ modules/workbenches/registry.ts
@@ -2,7 +2,7 @@ import {ModelerWorkspace} from "workbenches/modeler";
 import {ExampleWorkspace} from "workbenches/examples";
 import {WorkbenchConfig} from "cad/workbench/workbenchService";
 
+
 export const WorkbenchRegistry: WorkbenchConfig[] = [
     ModelerWorkspace,
-    // ExampleWorkspace,
 ]
