35b6366cc3fdde89c0c736e85caccd0ea0d82567
Mike Molinari <mike@smbisoft.com>
2022-05-13 04:27:14 +0000

    reduced size of wasm

--- web/app/cad/craft/e0/occtPlugin.ts
+++ web/app/cad/craft/e0/occtPlugin.ts
@@ -3,6 +3,7 @@ import {CraftEngine} from "./craftEngine";
 import {createOCCService, OCCService} from "cad/craft/e0/occService";
 
 
+
 declare module 'context' {
   interface CoreContext {
     occService: OCCService;
@@ -22,9 +23,17 @@ export function activate(ctx) {
   ctx.occService = createOCCService(ctx);
 }
 
-function instantiateEngine(importObject, callback) {
-  const url = './wasm/e0/main.wasm';
-  WebAssembly.instantiateStreaming(fetch(url), importObject).then(results => {
+async function instantiateEngine(importObject, callback) {
+  const url = './wasm/e0/main.wasm.gz';
+  const buffer = pako.ungzip(await(await fetch(url)).arrayBuffer());
+
+  // A fetched response might be decompressed twice on Firefox.
+  // See https://bugzilla.mozilla.org/show_bug.cgi?id=610679
+  if (buffer[0] === 0x1f && buffer[1] === 0x8b) {
+    buffer = ungzip(buffer);
+  }
+
+  const result = await WebAssembly.instantiate(buffer, importObject).then(results => {
     callback(results.instance);
   });
 }
