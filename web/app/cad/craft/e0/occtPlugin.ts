368991f0ce5ff894cd247232937c29be9cd0d4f2
Val Erastov <xibyte@gmail.com>
2022-06-05 22:32:25 -0700

    loading occ wasm from npm package

--- web/app/cad/craft/e0/occtPlugin.ts
+++ web/app/cad/craft/e0/occtPlugin.ts
@@ -3,7 +3,6 @@ import {CraftEngine} from "./craftEngine";
 import {createOCCService, OCCService} from "cad/craft/e0/occService";
 
 
-
 declare module 'context' {
   interface CoreContext {
     occService: OCCService;
@@ -23,17 +22,10 @@ export function activate(ctx) {
   ctx.occService = createOCCService(ctx);
 }
 
-async function instantiateEngine(importObject, callback) {
-  const url = './wasm/e0/main.wasm.gz';
-  const buffer = pako.ungzip(await(await fetch(url)).arrayBuffer());
+function instantiateEngine(importObject, callback) {
+  const url = './main.wasm';
 
-  // A fetched response might be decompressed twice on Firefox.
-  // See https://bugzilla.mozilla.org/show_bug.cgi?id=610679
-  if (buffer[0] === 0x1f && buffer[1] === 0x8b) {
-    buffer = ungzip(buffer);
-  }
-
-  const result = await WebAssembly.instantiate(buffer, importObject).then(results => {
+  WebAssembly.instantiateStreaming(fetch(url), importObject).then(results => {
     callback(results.instance);
   });
 }
@@ -56,7 +48,7 @@ function loadWasm(ctx) {
   } as any;
 
   let mainScript = document.createElement('script');
-  mainScript.setAttribute('src', './wasm/e0/main.js');
+  mainScript.setAttribute('src', './main.js');
   mainScript.setAttribute('async', 'async');
   document.head.appendChild(mainScript);
 }
