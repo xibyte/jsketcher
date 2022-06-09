2c510f1e1bfe3724a03ee003c3b46ff1e3ff0994
Mike Molinari <mike@smbisoft.com>
2022-06-09 01:31:02 +0000

    Change refrence geometry from grey to brown

--- web/app/cad/scene/views/sketchObjectView.js
+++ web/app/cad/scene/views/sketchObjectView.js
@@ -3,7 +3,7 @@ import {CurveBasedView} from './curveBasedView';
 export class SketchObjectView extends CurveBasedView {
   
   constructor(ctx, mSketchObject, sketchToWorldTransformation) {
-    const color = mSketchObject.construction ? 0x777777 : 0x0000FF;
+    const color = mSketchObject.construction ? 0x964B00 : 0x0000FF;
     const tess = mSketchObject.sketchPrimitive.tessellate(10).map(sketchToWorldTransformation.apply).map(v => v.data());
     super(ctx, mSketchObject, tess, 3, 4, color, 0x49FFA5, true);
     this.picker.onDblclick = () => {
