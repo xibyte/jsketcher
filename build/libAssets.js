99caf485eebffb924291008651cc20ba5fd58103
Val Erastov <xibyte@gmail.com>
2022-06-25 17:14:26 -0700

    lib assets support

---
+++ build/libAssets.js
@@ -0,0 +1,14 @@
+
+module.exports = [
+
+  'jsketcher-occ-engine/occt.js',
+  'jsketcher-occ-engine/occt.wasm',
+
+  'font-awesome/css/font-awesome.min.css',
+  'font-awesome/fonts/FontAwesome.otf',
+  'font-awesome/fonts/fontawesome-webfont.eot',
+  'font-awesome/fonts/fontawesome-webfont.svg',
+  'font-awesome/fonts/fontawesome-webfont.ttf',
+  'font-awesome/fonts/fontawesome-webfont.woff',
+  'font-awesome/fonts/fontawesome-webfont.woff2',
+]
