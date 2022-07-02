023abf177ad1b61f82a9eb4d486175c947c34fd3
Mike Molinari <mike@smbisoft.com>
2022-07-02 00:25:51 +0000

    added inteligent IDs to loft opperation

--- modules/workbenches/modeler/features/loft/loft.operation.ts
+++ modules/workbenches/modeler/features/loft/loft.operation.ts
@@ -19,10 +19,25 @@ export const LoftOperation: OperationDescriptor<LoftParams> = {
   icon: 'img/cad/loft',
   info: 'Lofts 2D sketch',
   paramsInfo: ({ }) => `(${r()})`,
-  run: async (params: LoftParams, ctx: ApplicationContext) => {
+  run:async (params: LoftParams, ctx: ApplicationContext) => {
 
     let occ = ctx.occService;
     const oci = occ.commandInterface;
+
+
+
+    console.log(params.loops);
+
+    let sketches = [];
+
+    const wires = params.loops.map((loop, i) => {
+      const shapeName = "loop/" + i;
+      sketches.push(loop.parent);
+
+      return occ.io.sketchLoader.pushContourAsWire(loop.contour, shapeName, loop.face.csys).wire
+    });
+
+    console.log("This is the info you are looking for", sketches);
 
     let loftType = 0;
     if (params.loftType == "smooth") loftType = 0;
@@ -30,6 +45,7 @@ export const LoftOperation: OperationDescriptor<LoftParams> = {
 
     console.log(params.loops);
 
+<<<<<<< HEAD
     let sketches = [];
 
     const wires = params.loops.map((loop, i) => {
@@ -47,23 +63,10 @@ export const LoftOperation: OperationDescriptor<LoftParams> = {
 
     let sweepSources = [];
 
-    let indexOfMostSegments = 0;
-    let longestPath =  0;
-    let primarySketch = {};
-
-    sketches.forEach(function (item, index) {
+    sketches.forEach(await async function (item, index) {
       console.log(item, index);
-      if(params.loops[index].contour.segments.length > longestPath){
-        longestPath = params.loops[index].contour.segments.length;
-
-        primarySketch = params.loops[index].parent;
-      }
-      const face = occ.utils.sketchToFaces(ctx.sketchStorageService.readSketch(item.id), item.csys);
-      sweepSources = face;
+      await sweepSources.concat(await occ.utils.sketchToFaces(ctx.sketchStorageService.readSketch(item.id), item.csys))
     });
-
-
-    
 
     const productionAnalyzer = new FromSketchProductionAnalyzer(sweepSources);
 
@@ -74,7 +77,7 @@ export const LoftOperation: OperationDescriptor<LoftParams> = {
     let tools = [];
     tools.push(occ.io.getShell("th", productionAnalyzer));
 
-    return occ.utils.applyBooleanModifier(tools, params.boolean, [], [],)
+    return occ.utils.applyBooleanModifier(tools, params.boolean, sketches, [],)
 
   },
 
