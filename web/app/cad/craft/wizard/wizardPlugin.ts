d9a1c8c43430fb4cb1d60e04144b4306cb7c81b4
Val Erastov <xibyte@gmail.com>
2022-07-24 14:28:24 -0700

    explicitly set active field by default

--- web/app/cad/craft/wizard/wizardPlugin.ts
+++ web/app/cad/craft/wizard/wizardPlugin.ts
@@ -91,17 +91,16 @@ export function activate(ctx: ApplicationContext) {
       disposerList = createFunctionList();
     }
 
-    if (curr !== null && old?.requestKey !== curr.requestKey) {
-      const newState: WizardState = {};
-      if (curr) {
-        const op = ctx.operationService.get(curr.type);
-        if (op.defaultActiveField) {
-          newState.activeParam = op.defaultActiveField;
-        }
+    const newState: WizardState = {};
+
+    if (curr) {
+      const op = ctx.operationService.get(curr.type);
+      if (op.defaultActiveField) {
+        newState.activeParam = op.defaultActiveField;
       }
-      state$.next(newState);
     }
 
+    state$.next(newState);
   })
 
   const updateParams = mutator => workingRequest$.update((req: WorkingRequest) => produce(req, draft => {
