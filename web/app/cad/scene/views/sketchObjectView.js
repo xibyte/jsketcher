bb234a7c573f81b43fc2d7f4013e3a2a2e4cc12f
Val Erastov <xibyte@gmail.com>
2022-08-30 19:28:06 -0700

    calculating tesselation scale

--- web/app/cad/scene/views/sketchObjectView.js
+++ web/app/cad/scene/views/sketchObjectView.js
@@ -4,7 +4,12 @@ export class SketchObjectView extends CurveBasedView {
   
   constructor(ctx, mSketchObject, sketchToWorldTransformation) {
     const color = mSketchObject.construction ? 0x964B00 : 0x0000FF;
-    const tess = mSketchObject.sketchPrimitive.tessellate(10).map(sketchToWorldTransformation.apply).map(v => v.data());
+
+    const massiveness = mSketchObject.sketchPrimitive.massiveness();
+
+    const MAGIC_RATIO = 0.01;
+    const tess = mSketchObject.sketchPrimitive.tessellate(massiveness * MAGIC_RATIO)
+      .map(sketchToWorldTransformation.apply).map(v => v.data());
     super(ctx, mSketchObject, tess, 3, color);
     this.representation.onDblclick = () => {
       ctx.sketcherService.sketchFace(this.model.face);
