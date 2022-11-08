ffc56450283ac5cc0fb0c59743f05a54466c5163
Mike Molinari <mike@smbisoft.com>
2022-11-08 23:27:01 +0000

    removed left over debugging alert

--- web/app/cad/scene/viewer.ts
+++ web/app/cad/scene/viewer.ts
@@ -65,7 +65,6 @@ export default class Viewer {
   }
   
   toggleCamera() {
-    alert("camera toggled")
     if (this.getCameraMode() === CAMERA_MODE.PERSPECTIVE) {
       this.setCameraMode(CAMERA_MODE.ORTHOGRAPHIC);
     } else {
